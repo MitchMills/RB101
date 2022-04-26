5d1c8e5e9431cdcda1393a7d9590b9d1b6eeecad
Mitch Mills <mitchmillsgcp@gmail.com>
2022-04-26 15:46:34 -0500

    Tinkered

--- 02Small_Programs/Loan_Calculator/16loan_calculator2_sandbox.rb
+++ 02Small_Programs/Loan_Calculator/16loan_calculator2_sandbox.rb
@@ -1,3 +1,6 @@
+# Build a mortgage / car payment calculator that takes as input: loan amount, APR, loan duration
+# Calculate: monthly interest rate, loan duration in months, monthly payment amount
+
 require 'yaml'
 MESSAGES = YAML.load_file('16loan_calculator2_messages.yml')
 
@@ -26,55 +29,141 @@ def valid_integer?(input)
   integer?(input) && non_negative?(input)
 end
 
-##################
-
-YES = ['y', 'yes']
-NO = ['n', 'no']
-
-def format_loan_amount(loan_amount)
-  loan_amount
+# name = ''
+def get_name
+  loop do
+    prompt('get_name')
+    name = gets.chomp
+    if name.empty?
+      prompt('invalid_name')
+    else
+      return name
+    end
+  end
 end
+name = get_name
+puts "Hi, #{name}!"
 
-# def format_apr(apr)
+# def welcome
+#   prompt('welcome')
+#   name = get_name
+#   prompt('instructions', name)
 # end
 
-# def format_loan_duration_months(loan_duration_months)
+# def welcome_back(name)
+#   prompt('welcome_back', name)
+#   prompt('instructions_again')
 # end
 
-# def confirm_input(loan_amount)
+# first_time? = true
+# def intro(first_time?)
+#   system 'clear'
+#   if first_time?
+#     welcome
+#     first_time? = false
+#   else
+#     welcome_back
+#   end
+# end
+
+# # loan_amount = ''
+# def get_loan_amount
 #   loop do
-#     formatted_loan_amount = format_loan_amount(loan_amount)
-#     prompt('confirm_input', formatted_loan_amount)
-#     answer = gets.chomp.downcase
-#     if YES.include?(answer)
-#       return
-#     elsif NO.include?(answer)
-#       get_loan_amount
+#     prompt('loan_amount')
+#     loan_amount = gets.chomp
+#     if valid_number?(loan_amount)
+#       return loan_amount.to_f
 #     else
-#       prompt('enter_yes_or_no')
+#       prompt('invalid_loan_amount')
 #     end
 #   end
 # end
 
-def correct_input?(loan_amount)
-  formatted_loan_amount = format_loan_amount(loan_amount)
-  prompt('confirm_input', formatted_loan_amount)
-  answer = gets.chomp.downcase
-  YES.include?(answer)
-end
+# def get_apr
+#   loop do
+#     prompt('apr')
+#     apr = gets.chomp
+#     if valid_number?(apr)
+#       return apr.to_f
+#     else
+#       prompt('invalid_entry')
+#     end
+#   end
+# end
 
-loan_amount = ''
-def get_loan_amount
-  loop do
-    prompt('loan_amount')
-    loan_amount = gets.chomp
-    if valid_number?(loan_amount)
-      confirm_input(loan_amount)
-      return loan_amount.to_f
-    else
-      prompt('invalid_loan_amount')
-    end
-  end
-end
+# def calculate_mpr(apr)
+#   (apr / 12) / 100
+# end
 
-p get_loan_amount
+# def get_years_in_loan
+#   loop do
+#     prompt('whole_years_in_loan')
+#     years_in_loan = gets.chomp
+#     if valid_integer?(years_in_loan)
+#       return years_in_loan.to_i
+#     else
+#       prompt('invalid_entry')
+#     end
+#   end
+# end
+
+# def get_months_in_loan
+#   loop do
+#     prompt('additional_months_in_loan')
+#     months_in_loan = gets.chomp
+#     if valid_integer?(months_in_loan)
+#       return months_in_loan.to_i
+#     else
+#       prompt('invalid_entry')
+#     end
+#   end
+# end
+
+# def calculate_loan_duration_months(loan_years, loan_months)
+#   (loan_years * 12) + loan_months
+# end
+
+# def calculate_monthly_payment(loan_amount, mpr, loan_duration_months)
+#   loan_amount *
+#   (mpr /
+#   (1 - (1 + mpr)**(-loan_duration)))
+# end
+
+# def calculate_total_interest(monthly_payment, loan_duration_months, loan_amount)
+#   (monthly_payment * loan_duration_months) - loan_amount
+# end
+
+# def display_results(loan_duration_months, monthly_payment, total_interest)
+#   prompt('number_of_payments', loan_duration_months)
+#   prompt('monthly_payment', monthly_payment)
+#   prompt('total_interest', total_interest)
+# end
+
+# def another_calculation?
+#   loop do
+#     prompt('another_calculation?', name)
+#     answer = gets.chomp
+    
+#   end
+# end
+
+
+# # Main Loop
+# loop do
+#   intro(first_time?)
+#   loan_amount = get_loan_amount
+#   apr = get_apr
+#   mpr = calculate_mpr(apr)
+#   years_in_loan = get_years_in_loan
+#   months_in_loan = get_months_in_loan
+#   loan_duration_months = calculate_loan_duration_months
+#   monthly_payment = calculate_monthly_payment
+#   total_interest = calculate_total_interest
+#   display_results  
+#   if another_calculation?
+#     break
+#   else
+#     prompt('goodbye', name)
+#     exit
+#   end
+# end
