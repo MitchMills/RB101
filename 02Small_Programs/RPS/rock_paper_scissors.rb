1656cbbb0d7fd99cce4a93da829cf0e9dfa03395
Mitch Mills <mitchmillsgcp@gmail.com>
2022-08-21 14:02:14 -0500

    First attempt

---
+++ 02Small_Programs/RPS/rock_paper_scissors.rb
@@ -0,0 +1,45 @@
+VALID_CHOICES = %w(rock paper scissors)
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def display_results(player_choice, computer_choice)
+  prompt("You chose: #{player_choice}")
+  prompt("Computer chose: #{computer_choice}")
+
+  if  (player_choice == 'rock' && computer_choice == 'scissors') ||
+      (player_choice == 'paper' && computer_choice == 'rock') ||
+      (player_choice == 'scissors' && computer_choice == 'paper')
+    prompt("You won!")
+  elsif (player_choice == 'rock' && computer_choice == 'paper') ||
+        (player_choice == 'paper' && computer_choice == 'scissors') ||
+        (player_choice == 'scissors' && computer_choice == 'rock')
+    prompt("Computer won!")
+  else
+    prompt("It's a tie.")
+  end
+end
+
+loop do # main loop
+  player_choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    player_choice = gets.chomp
+    if VALID_CHOICES.include?(player_choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  display_results(player_choice, computer_choice)
+
+  prompt("Type y to play again, any other letter to quit.")
+  play_again = gets.chomp.downcase
+  break unless play_again == 'y'
+end
+
+prompt("Thank you for playing. Goodbye!")
