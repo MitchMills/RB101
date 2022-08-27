1e8a5bc46e991831b1e0f1a013475b8512ce143a
Mitch Mills <mitchmillsgcp@gmail.com>
2022-08-27 11:32:06 -0500

    Refactored methods

--- 02Small_Programs/RPS/rpsls.rb
+++ 02Small_Programs/RPS/rpsls.rb
@@ -33,40 +33,63 @@ def display_results(player_choice, computer_choice)
   end
 end
 
-def update_score(player_choice, computer_choice)
+def player_score(player_choice, computer_choice, player_score)
   if win?(player_choice, computer_choice)
-    player_score +=1
-  elsif win?(computer_choice, player_choice)
-    computer_score +=1
+    player_score += 1
+  else
+    player_score
   end
 end
 
-loop do # main loop
+def computer_score(player_choice, computer_choice, computer_score)
+  if win?(computer_choice, player_choice)
+    computer_score += 1
+  else
+    computer_score
+  end
+end
+
+# main game loop
+loop do
   player_choice = ''
   player_score = 0
   computer_score = 0
 
   prompt("Welcome to Rock Paper Scissors Spock Lizard!")
-  prompt("The first player to win three games will be the grand winner.")
+  prompt("The first player to win three games will be the Grand Winner.")
 
   loop do
     loop do
-        prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-        player_choice = gets.chomp
-        if VALID_CHOICES.include?(player_choice)
-          break
-        else
-          prompt("That's not a valid choice.")
-        end
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      player_choice = gets.chomp
+      if VALID_CHOICES.include?(player_choice)
+        break
+      else
+        prompt("That's not a valid choice.")
       end
-
-      computer_choice = VALID_CHOICES.sample
-
-      display_results(player_choice, computer_choice)
-
-      update_score(player_choice, computer_choice)
-
     end
+
+    computer_choice = VALID_CHOICES.sample
+
+    display_results(player_choice, computer_choice)
+
+    player_score = player_score(player_choice, computer_choice, player_score)
+    computer_score = computer_score(player_choice, computer_choice, computer_score)
+
+    if player_score == 3
+      prompt("You have won three games.")
+      prompt("You are the Grand Winner!")
+      break
+    elsif computer_score == 3
+      prompt("Computer has won three games.")
+      prompt("Computer is the Grand Winner!")
+      break
+    else
+      prompt("You have won #{player_score} games.")
+      prompt("Computer has won #{computer_score} games.")
+      prompt("Time for another round!")
+    end
+  end
 
   prompt("To play again, type y. Type any other letter to quit.")
   play_again = gets.chomp.downcase
