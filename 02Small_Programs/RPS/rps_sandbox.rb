f2dbc5ffb485db761c0ad0064d4bfc7359d98db8
Mitch Mills <mitchmillsgcp@gmail.com>
2022-08-30 16:14:09 -0500

    Polished user messages and program flow

--- 02Small_Programs/RPS/rps_sandbox.rb
+++ 02Small_Programs/RPS/rps_sandbox.rb
@@ -1,7 +1,37 @@
-loop do
-  system('clear')
-  name = 'bob'
-  puts "#{name}1"
-  gets
-  puts "#{name}2"
+CHOICES = {
+  'rock' => {
+    abbreviation: 'r',
+    beats: {'lizard' => 'crushes', 'scissors' => 'crushes'}
+  },
+  'paper' => {
+    abbreviation: 'p',
+    beats: {'rock' => 'covers', 'spock' => 'disproves'}
+  },
+  'scissors' => {
+    abbreviation: 'sc',
+    beats: {'paper' => 'cuts', 'lizard' => 'decapitates'}
+  },
+  'spock' => {
+    abbreviation: 'sp',
+    beats: {'scissors' => 'smashes', 'rock' => 'vaporizes'}
+  },
+  'lizard' => {
+    abbreviation: 'l',
+    beats: {'spock' => 'poisons', 'paper' => 'eats'}
+  }
+}
+
+VALID_CHOICES = CHOICES.keys.map {|key| CHOICES[key][:abbreviation]}
+
+RULES = CHOICES.map do |key, value|
+  "#{key.capitalize} (#{value[:abbreviation]}) defeats:\
+  #{value[:beats].keys.join(', ')}"
 end
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+prompt("Line 1\n")
+prompt("Line 2\n\n")
+prompt("Line 3")
--- 02Small_Programs/RPS/rpsls.rb
+++ 02Small_Programs/RPS/rpsls.rb
@@ -1,32 +1,36 @@
 CHOICES = {
   'rock' => {
     abbreviation: 'r',
-    beats: {'lizard' => 'crushes', 'scissors' => 'crushes'}
+    beats: { 'lizard' => 'crushes', 'scissors' => 'crushes' }
   },
   'paper' => {
     abbreviation: 'p',
-    beats: {'rock' => 'covers', 'spock' => 'disproves'}
+    beats: { 'rock' => 'covers', 'spock' => 'disproves' }
   },
   'scissors' => {
     abbreviation: 'sc',
-    beats: {'paper' => 'cuts', 'lizard' => 'decapitates'}
+    beats: { 'paper' => 'cuts', 'lizard' => 'decapitates' }
   },
   'spock' => {
     abbreviation: 'sp',
-    beats: {'scissors' => 'smashes', 'rock' => 'vaporizes'}
+    beats: { 'scissors' => 'smashes', 'rock' => 'vaporizes' }
   },
   'lizard' => {
     abbreviation: 'l',
-    beats: {'spock' => 'poisons', 'paper' => 'eats'}
+    beats: { 'spock' => 'poisons', 'paper' => 'eats' }
   }
 }
 
-VALID_CHOICES = CHOICES.keys.map {|key| CHOICES[key][:abbreviation]}
+VALID_CHOICES = CHOICES.keys.map { |key| CHOICES[key][:abbreviation] }
+
+RULES = CHOICES.map do |key, value|
+  "#{key.capitalize} (#{value[:abbreviation]}) defeats:\
+  #{value[:beats].keys.join(', ')}"
+end
 
 def prompt(message)
   puts "=> #{message}"
 end
-
 
 def get_name
   loop do
@@ -35,50 +39,49 @@ def get_name
     if name.empty?
       prompt("That's not a valid entry.")
     else
+      puts
       return name
     end
   end
 end
 
 def welcome(name)
-  prompt("Thanks, #{name}. You will play against the computer.")
-  prompt("The first to win three games will be declared the Grand Winner.")
+  prompt("Greetings, #{name}. Your opponent is Computer.")
+  prompt("The first of you to win three games will be declared \
+    the Grand Winner.")
   prompt("To display the rules, enter y. Enter any other letter to continue.")
   show_rules = gets.chomp.downcase
-  if show_rules == 'y'
-    display_rules
-    prompt("Okay, let's start playing!")
-  else
-    prompt("Let's get started!")
+
+  display_rules if show_rules == 'y'
+  prompt("Okay, let's start playing! Your choices are:")
+  CHOICES.each do |key, value|
+    prompt("#{key.capitalize} (#{value[:abbreviation]})")
   end
+  puts
+end
+
+def welcome_back(name)
+  prompt("Welcome back, #{name}! Let's see who'll be \
+    the Grand Winner this time.")
+  prompt("To display the rules again, enter y. Enter \
+    any other letter to continue.")
+  show_rules = gets.chomp.downcase
+
+  display_rules if show_rules == 'y'
+  prompt("Time for another match! A quick reminder of your choices:")
+  CHOICES.each do |key, value|
+    prompt("#{key.capitalize} (#{value[:abbreviation]})")
+  end
+  puts
 end
 
 def display_rules
-  rules = <<~RULES
-    Rock defeats: Lizard, Scissors
-       Paper defeats: Rock, Spock
-       Scissors defeats: Paper, Lizard
-       Spock defeats: Scissors, Rock
-       Lizard defeats: Spock, Paper
-
-    RULES
-  prompt(rules)
+  RULES.each { |rule| prompt(rule.to_s) }
+  puts
   prompt("Press enter to continue")
   gets
 end
 
-def welcome_back(name)
-  prompt("Welcome back, #{name}! Ready for another match?")
-  prompt("To display the rules again, enter y. Enter any other letter to continue.")
-  show_rules = gets.chomp.downcase
-  if show_rules == 'y'
-    display_rules
-  else
-    prompt("Let's go!")
-  end
-end
-
-first_time = true
 def game_intro(first_time, name)
   if first_time
     welcome(name)
@@ -86,8 +89,6 @@ def game_intro(first_time, name)
     welcome_back(name)
   end
 end
-
-
 
 def abbreviation_to_word(player_choice)
   CHOICES.each do |word, value|
@@ -97,12 +98,12 @@ end
 
 def get_player_choice
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt("Enter one: #{VALID_CHOICES.join(', ')}")
     player_choice = gets.chomp.strip.downcase
     if VALID_CHOICES.include?(player_choice)
       return abbreviation_to_word(player_choice)
     else
-      prompt("That's not a valid choice.")
+      prompt("That's not a valid entry.")
     end
   end
 end
@@ -126,19 +127,16 @@ def print_outcome(player1, player2)
   prompt("#{player1.capitalize} #{verb} #{player2}.")
 end
 
-def display_results(player_choice, computer_choice, winner)
-  prompt("You chose #{player_choice}, computer chose #{computer_choice}.")
+def display_results(name, player_choice, computer_choice, winner)
+  prompt("#{name} chose #{player_choice}, Computer chose #{computer_choice}.")
   if winner == 'player'
     print_outcome(player_choice, computer_choice)
-    prompt("You won!")
-    puts
+    prompt("#{name} won!\n\n")
   elsif winner == 'computer'
     print_outcome(computer_choice, player_choice)
-    prompt("Computer won!")
-    puts
+    prompt("Computer won!\n\n")
   else
-    prompt("It's a tie.")
-    puts
+    prompt("It's a tie.\n\n")
   end
 end
 
@@ -153,9 +151,9 @@ def update_scores(winner, scores)
   scores
 end
 
-def display_scores(scores)
+def display_scores(scores, name)
   prompt("CURRENT SCORE")
-  prompt("Player: #{scores[:player_score]}")
+  prompt("#{name}: #{scores[:player_score]}")
   prompt("Computer: #{scores[:computer_score]}")
   prompt("Ties: #{scores[:ties]}")
   puts
@@ -165,34 +163,32 @@ def grand_winner?(scores)
   scores[:player_score] == 3 || scores[:computer_score] == 3
 end
 
-def display_grand_winner(scores)
+def display_grand_winner(scores, name)
   if scores[:player_score] == 3
-    prompt("You have won three games. You are the Grand Winner!")
+    prompt("You have won three games, #{name}! You are the Grand Winner!")
   elsif scores[:computer_score] == 3
     prompt("Computer has won three games. Computer is the Grand Winner!")
   end
 end
 
-def play_again
+def play_again(name)
   prompt("To play another match, enter y. Enter any other letter to quit.")
   play_again = gets.chomp.downcase
   unless play_again == 'y'
-    prompt("Thank you for playing. Goodbye!") 
+    prompt("Thank you for playing, #{name}. Goodbye!")
     exit
   end
 end
 
-
-# 
-# 
-# 
-# 
 # main program loop
+first_time = true
+name = ''
+
 loop do
   system('clear')
   player_choice = ''
-  scores = {player_score: 0, computer_score: 0, ties: 0}
-  
+  scores = { player_score: 0, computer_score: 0, ties: 0 }
+
   if first_time
     prompt("Welcome to Rock Scissors Paper Spock Lizard!")
     name = get_name
@@ -207,16 +203,16 @@ loop do
 
     winner = determine_winner(player_choice, computer_choice)
 
-    display_results(player_choice, computer_choice, winner)
+    display_results(name, player_choice, computer_choice, winner)
 
     scores = update_scores(winner, scores)
-    display_scores(scores)
+    display_scores(scores, name)
 
     if grand_winner?(scores)
-      display_grand_winner(scores)
-      break    
+      display_grand_winner(scores, name)
+      break
     end
   end
 
-  play_again()
+  play_again(name)
 end
