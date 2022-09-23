c1c72694754c6efd1ac04a004322000da444647f
Mitch Mills <mitchmillsgcp@gmail.com>
2022-09-23 15:37:17 -0500

    Worked through palindrome substrings problem

--- 04Ruby_Collections/palindrome_substrings.rb
+++ 04Ruby_Collections/palindrome_substrings.rb
@@ -1,19 +1,32 @@
-def substrings(str)
-  result = []
-  starting_index = 0;
-
-  while (starting_index <= str.length - 2)
-    num_chars = 2
-    while (num_chars <= str.length - starting_index)
-      substring = str.slice(starting_index, num_chars)ß
-      result << substring
-      num_chars += 1
+def get_substrings(string)
+  starting_index = 0
+  substrings = []
+  while starting_index <= (string.length - 2)
+    substring_length = 2
+    while substring_length <= (string.length - starting_index)
+      current_substring = string.slice(starting_index, substring_length)
+      substrings << current_substring
+      substring_length += 1
     end
     starting_index += 1
   end
-  result
+  substrings
 end
 
-string = 'goalies'
-substrings = substrings(string)
-p substrings
+def palindrome?(substring)
+  substring == substring.reverse
+end
+
+def palindrome_substrings(string)
+  palindromes = []
+  substrings = get_substrings(string)
+  substrings.each do |substring|
+    palindromes << substring if palindrome?(substring)
+  end
+  palindromes
+end
+
+puts palindrome_substrings("supercalifragilisticexpialidocious") == ["ili"]
+puts palindrome_substrings("abcddcbA") == ["bcddcb", "cddc", "dd"]
+puts palindrome_substrings("palindrome") == []
+puts palindrome_substrings("") == []
