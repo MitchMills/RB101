921805ffa30f437385a28f96276d7b8b6ee9cd8c
Mitch Mills <mitchmillsgcp@gmail.com>
2022-10-07 13:16:47 -0500

    Practiced select & transform examples

--- 04Ruby_Collections/select_transform.rb
+++ 04Ruby_Collections/select_transform.rb
@@ -1,19 +1,121 @@
 # Selection and Transformation Practice
 
-# Extracting to Methods, Hash
-def select_fruit
-  selected_fruit = {}
-  
+# Select then transform
+def double_odd_indices(numbers)
+  doubled_numbers = []
+  counter = 0
+
+  loop do
+    break if counter == numbers.size
+
+    current_number = numbers[counter]
+    current_number *= 2 if counter.odd?
+    doubled_numbers << current_number
+
+    counter += 1
+  end
+
+  doubled_numbers
 end
 
-produce = {
-  'apple' => 'Fruit',
-  'carrot' => 'Vegetable',
-  'pear' => 'Fruit',
-  'broccoli' => 'Vegetable'
-}
+my_numbers = [1, 4, 3, 7, 2, 6]
+p my_numbers
+p double_odd_indices(my_numbers)
+p my_numbers
 
-select_fruit(produce)
+# def double_odd_numbers(numbers)
+#   doubled_numbers = []
+#   counter = 0
+
+#   loop do
+#     break if counter == numbers.size
+
+#     current_number = numbers[counter]
+#     current_number *= 2 if current_number.odd?
+#     doubled_numbers << current_number
+
+#     counter += 1
+#   end
+
+#   doubled_numbers
+# end
+
+# my_numbers = [1, 4, 3, 7, 2, 6]
+# p my_numbers
+# p double_odd_numbers(my_numbers)
+# p my_numbers
+
+# Transformation within a method: mutating
+# def double_numbers!(numbers)
+#   counter = 0
+
+#   loop do
+#     break if counter == numbers.size
+#     numbers[counter] *= 2
+#     counter += 1
+#   end
+
+#   numbers
+# end
+
+# my_numbers = [1, 4, 3, 7, 2, 6]
+# p my_numbers
+# p double_numbers!(my_numbers)
+# p my_numbers
+
+# Transformation within a method: non-mutating
+# def double_numbers(numbers)
+#   doubled_numbers = []
+#   counter = 0
+
+#   loop do
+#     break if counter == numbers.size
+
+#     current_number = numbers[counter]
+#     doubled_numbers << current_number * 2
+
+#     counter += 1
+#   end
+
+#   doubled_numbers
+# end
+
+# my_numbers = [1, 4, 3, 7, 2, 6]
+# p my_numbers
+# p double_numbers(my_numbers)
+# p my_numbers
+
+# Extracting to Methods, Hash
+# def select_fruit(produce)
+#   selected_fruit = {}
+#   produce_keys = produce.keys
+#   index = 0
+
+#   loop do
+#     break if index == produce.size
+
+#     current_key = produce_keys[index]
+#     current_value = produce[current_key]
+
+#     if current_value == 'Fruit'
+#       selected_fruit[current_key] = current_value 
+#     end
+
+#     index += 1
+#   end
+
+#   selected_fruit  
+# end
+
+# produce = {
+#   'apple' => 'Fruit',
+#   'carrot' => 'Vegetable',
+#   'pear' => 'Fruit',
+#   'broccoli' => 'Vegetable'
+# }
+
+# p produce
+# p select_fruit(produce)
 
 # Extracting to Methods, Array
 # def select_vowels(string)
