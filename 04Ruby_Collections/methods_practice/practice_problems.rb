86c9a9896a61d341bd39d455442743ab4c19daa5
Mitch Mills <mitchmillsgcp@gmail.com>
2022-10-11 16:16:19 -0500

    Finished first Practice Problems

--- 04Ruby_Collections/methods_practice/practice_problems.rb
+++ 04Ruby_Collections/methods_practice/practice_problems.rb
@@ -1,10 +1,52 @@
 # Practice Problems: Methods and More Methods
 
+# Problem 10
+nums = [1, 2, 3]
+mapped_nums = nums.map do |num|
+                if num > 1
+                  puts num
+                else
+                  num
+                end
+              end
+p mapped_nums # --> [1, nil, nil]
+p nums # --> [1, 2, 3]
+
+# Problem 9
+# hash = { a: 'ant', b: 'bear' }
+# mapped_hash = hash.map do |key, value|
+#                 if value.size > 3
+#                   value
+#                 end
+#               end
+# p mapped_hash # --> [nil, 'bear']
+# p hash # --> { a: 'ant', b: 'bear }
+
+# Problem 8
+# array = [1, 2, 3, 4, 5]
+# taken = array.take(2)
+# p taken # --> [1, 2]
+# p array # --> [1, 2, 3, 4, 5]
+
+# Problem 7
+# nums = [1, 2, 3]
+# nums_any =  nums.any? do |num|
+#               puts num
+#               num.odd? # --> [true, false, true]
+#             end # --> 1 (iteration stops on first true returned)
+# p nums_any # --> true
+# p nums
+
+# Problem 6
+# animals = ['ant', 'bear', 'caterpillar']
+# p animals.pop.size # --> 11
+# p animals
+
 # Problem 5
-hash = { a: 'ant', b: 'bear' }
-shifted = hash.shift
-p shifted # --> [:a, 'ant]
-p hash # --> { b: 'bear' }
+# hash = { a: 'ant', b: 'bear' }
+# shifted = hash.shift
+# p shifted # --> [:a, 'ant]
+# p hash # --> { b: 'bear' }
 
 # Problem 4
 # animals = ['ant', 'bear', 'cat']
