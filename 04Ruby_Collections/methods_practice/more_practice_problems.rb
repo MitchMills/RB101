7b4a98677fd9236e75e653879993069428cf01e6
Mitch Mills <mitchmillsgcp@gmail.com>
2022-10-13 16:53:08 -0500

    Worked on second Practice Problems

---
+++ 04Ruby_Collections/methods_practice/more_practice_problems.rb
@@ -0,0 +1,48 @@
+
+# PROBLEM 3
+ages = { 'Herman' => 32, 'Lily' => 30, 'Grandpa' => 402, 'Eddie' => 10 }
+
+# Solution 3.1
+# youngs_only = ages.select! { |name, age| age < 100 }
+
+# Solution 3.2
+youngs_only = ages.keep_if { |name, age| age < 100 }
+p youngs_only
+
+
+# PROBLEM 2
+# ages = { 
+#         'Herman' => 32, 
+#         'Lily' => 30, 
+#         'Grandpa' => 5_843, 
+#         'Eddie' => 10, 
+#         'Marilyn' => 22, 
+#         'Spot' => 237 
+#       }
+
+# Solution 2.1
+# cumulative_ages = 0
+# ages.each do |name, age|
+#   cumulative_ages += age
+# end
+
+# Solution 2.2
+# cumulative_ages = ages.values.inject(:+)
+# p cumulative_ages
+
+
+# PROBLEM 1
+# flintstones = ['Fred', 'Barney', 'Wilma', 'Betty', 'Pebbles', 'BamBam']
+
+# Solution 1.1
+# flinthash = {}
+# flintstones.each_with_index do |name, index|
+#   flinthash[name] = index
+# end
+
+# Solution 1.2
+# flinthash = flintstones.each_with_object({}) do |name, hash|
+#   hash[name] = flintstones.index(name)
+# end
+
+# p flinthash
