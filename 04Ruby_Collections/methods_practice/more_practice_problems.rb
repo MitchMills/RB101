543a04b18d153dff69b4c00be1bcf25fc2eb283d
Mitch Mills <mitchmillsgcp@gmail.com>
2022-10-14 11:16:06 -0500

    Worked on second Practice Problems

--- 04Ruby_Collections/methods_practice/more_practice_problems.rb
+++ 04Ruby_Collections/methods_practice/more_practice_problems.rb
@@ -1,13 +1,35 @@
 
+# PROBLEM 5
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+first_be =  flintstones.find_index do |name|
+              name.start_with?('Be')
+            end
+p first_be
+
+# PROBLEM 4
+# ages = {  'Herman' => 32,
+#           'Lily' => 30,
+#           'Grandpa' => 5843,
+#           'Eddie' => 10,
+#           'Marilyn' => 22,
+#           'Spot' => 237
+#         }
+
+# youngest_age = ages.values.min
+# youngest_person = ages.key(youngest_age)    
+# p youngest_person
+
+
+
 # PROBLEM 3
-ages = { 'Herman' => 32, 'Lily' => 30, 'Grandpa' => 402, 'Eddie' => 10 }
+# ages = { 'Herman' => 32, 'Lily' => 30, 'Grandpa' => 402, 'Eddie' => 10 }
 
 # Solution 3.1
 # youngs_only = ages.select! { |name, age| age < 100 }
 
 # Solution 3.2
-youngs_only = ages.keep_if { |name, age| age < 100 }
-p youngs_only
+# youngs_only = ages.keep_if { |name, age| age < 100 }
+# p youngs_only
 
 
 # PROBLEM 2
