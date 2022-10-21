f3d966968bc5b014df15c3460d7c6745f77798f7
Mitch Mills <mitchmillsgcp@gmail.com>
2022-10-21 11:21:34 -0500

    Finished Easy 1 Smally Problems

--- 00Small_Problems_Exercises/easy1.rb
+++ 00Small_Problems_Exercises/easy1.rb
@@ -1,14 +1,145 @@
-# 4 HOW MANY?
-def count_occurrences(vehicle)
-  
+# 10 WHAT'S MY BONUS
+def calculate_bonus(salary, bonus_status)
+  bonus_status ? (salary / 2.0) : 0
 end
 
-vehicles = [
-  'car', 'car', 'truck', 'car', 'SUV', 'truck',
-  'motorcycle', 'motorcycle', 'car', 'truck'
-]
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
 
-count_occurrences(vehicles)
+
+# 9 SUM OF DIGITS
+# def sum(integer)
+#   integer.digits.sum
+# end
+
+# def sum(integer)
+#   str_array = integer.to_s.chars
+#   int_array = str_array.map { |str| str.to_i }
+#   sum = int_array.reduce(:+)
+# end
+
+# def sum(integer)
+#   sum = 0
+
+#   str_array = integer.to_s.chars
+
+#   str_array.each do |digit|
+#     sum += digit.to_i
+#   end
+
+#   sum
+# end
+
+# puts sum(23) == 5
+# puts sum(496) == 19
+# puts sum(123_456_789) == 45
+
+
+# 8 ARRAY AVERAGE
+# def average(array)
+#   array.sum / array.size.to_f
+# end
+
+# puts average([1, 6]) #== 3
+# puts average([1, 5, 87, 45, 8, 8]) #== 25
+# puts average([9, 47, 23, 95, 16, 52]) #== 40
+
+
+# 7 STRINGY STRINGS
+# def stringy(integer, start = 1)
+#   output_string = ''
+
+#   integer.times do |index|
+#     if start == 0
+#       number = index.even? ? '0' : '1'
+#     else
+#       number = index.even? ? '1' : '0'
+#     end
+#     output_string << number
+#   end
+
+#   output_string
+# end
+
+# def stringy(integer)
+#   output_string = ''
+#   (1..integer).each do |int|
+#     if int.odd?
+#       output_string << '1'
+#     else
+#       output_string << '0'
+#     end
+#   end
+#   output_string
+# end
+
+# def stringy(integer)
+#   output_string = ''
+#   index = 0
+#   while output_string.size < integer
+#     if index.even?
+#       output_string << '1'
+#     else
+#       output_string << '0'
+#     end
+#     index += 1
+#   end
+#   output_string
+# end
+
+# puts stringy(6, 0) #== '101010'
+# puts stringy(9, 1) #== '101010101'
+# puts stringy(4, 2) #== '1010'
+# puts stringy(7, 0) #== '1010101'
+
+
+# 6 REVERSE IT (Part 2)
+# def reverse_words(string)
+#   words = string.split
+
+#   words.each do |word|
+#     word.reverse! if word.size >= 5
+#   end
+
+#   words.join(' ')
+# end
+
+# puts reverse_words('Professional') == 'lanoisseforP'
+# puts reverse_words('Walk around the block') == 'Walk dnuora the kcolb'
+# puts reverse_words('Launch School') == 'hcnuaL loohcS'
+
+
+# 5 REVERSE IT (Part 1)
+# def reverse_sentence(sentence)
+#   sentence.split.reverse.join(' ')
+# end
+
+# puts reverse_sentence('Hello World') == 'World Hello'
+# puts reverse_sentence('Reverse these words') == 'words these Reverse'
+# puts reverse_sentence('') == ''
+# puts reverse_sentence('    ') == '' # Any number of spaces results in ''
+
+
+# 4 HOW MANY?
+# def count_occurrences(vehicles)
+#   vehicle_counts = {}
+
+#   vehicles.uniq.each do |vehicle|
+#     vehicle_counts[vehicle] = vehicles.count(vehicle)
+#   end
+
+#   vehicle_counts.each do |vehicle, count|
+#     puts "#{vehicle} => #{count}"
+#   end
+# end
+
+# vehicles = [
+#   'car', 'car', 'truck', 'car', 'SUV', 'truck',
+#   'motorcycle', 'motorcycle', 'car', 'truck'
+# ]
+
+# count_occurrences(vehicles)
 
 
 # 3 LIST OF DIGITS
--- 00Small_Problems_Exercises/pedac_scratchpad.md
+++ 00Small_Problems_Exercises/pedac_scratchpad.md
@@ -1,23 +1,25 @@
 # PROBLEM
-- Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
+- Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
 
-INPUT: positive integer
-OUTPUT: array of integers
+*INPUT*: integer, boolean
+*OUTPUT*: integer (float?)
 
 **Rules/Requirements**
-- array maintains the order of the digits in the input integer
+- input integer will be positive
+- input integer represents salary
+- boolean represents whether bonus was earned
+- output integer (float?) represents bonus amount
 
 # EXAMPLES
-puts digit_list(12345) == [1, 2, 3, 4, 5]
-puts digit_list(7) == [7]
-puts digit_list(375290) == [3, 7, 5, 2, 9, 0]
-puts digit_list(444) == [4, 4, 4]
+calculate_bonus(2800, true) == 1400
+calculate_bonus(1000, false) == 0
+calculate_bonus(50000, true) == 25000
 
 # DATA STRUCTURE(S)
 - integer
-- array
+- boolean
 
 # ALGORITHM
-1. Iterate over the input integer
-2. Put each separate digit into an array 'digits'
-3. Return the array
+1. If boolean is false, return 0
+2. If boolean is true, return salary / 2
+
