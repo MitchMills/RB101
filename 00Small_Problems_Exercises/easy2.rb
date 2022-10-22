df541f7383e13d51c3e93a90c08f4258a928475a
Mitch Mills <mitchmillsgcp@gmail.com>
2022-10-22 13:01:54 -0500

    Worked on Easy 2 Small Problems

---
+++ 00Small_Problems_Exercises/easy2.rb
@@ -0,0 +1,43 @@
+
+# 2 HOW BIG IS THE ROOM?
+# SQMETERS_TO_SQFEET = 10.7639
+
+# def get_length_in_meters
+#   print "Enter the length of the room in meters: "
+#   length = gets.chomp.to_f
+# end
+
+# def get_width_in_meters
+#   print "Enter the width of the room in meters: "
+#   width = gets.chomp.to_f
+# end
+
+# def calculate_area_in_meters
+#   length = get_length_in_meters
+#   width = get_width_in_meters
+#   area_in_meters = (length * width).round(2)
+# end
+
+# def display_area
+#   area_in_meters = calculate_area_in_meters
+#   area_in_feet = (area_in_meters * SQMETERS_TO_SQFEET).round(2)
+#   puts "The area of the room is #{area_in_meters} square meters (#{area_in_feet} square feet)."
+# end
+
+# display_area
+
+
+# 1 HOW OLD IS TEDDY?
+# def age_of_somebody
+#   name = get_name
+#   age = rand(20..200)
+#   puts "#{name} is #{age} years old!"
+# end
+
+# def get_name
+#   print "Please enter a name: "
+#   name = gets.chomp
+#   name.empty? ? 'Teddy' : name
+# end
+
+# age_of_somebody
