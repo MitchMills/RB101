e69e67f7f2bf207416b0f4ba91810f17ff13a9ff
Mitch Mills <mitchmillsgcp@gmail.com>
2022-10-25 09:56:55 -0500

    Worked on Easy 3 Small Problems

---
+++ 00Small_Problems_Exercises/easy3.rb
@@ -0,0 +1,39 @@
+# 3 COUNTING THE NUMBER OF CHARACTERS
+
+
+
+# 2 ARITHMETIC INTEGER
+# print "==> Enter the first number: "
+# first_number = gets.to_i
+# print "==> Enter the second number: "
+# second_number = gets.to_i
+
+# operators = [:+, :-, :*, :/, :%, :**]
+
+# operators.each do |op|
+#   puts "#{first_number} #{op} #{second_number} = #{first_number.method(op).(second_number)}"
+# end
+
+
+# 1 SEARCHING 101
+# numbers_hash = { 
+#   '1st' => 0,
+#   '2nd' => 0,
+#   '3rd' => 0,
+#   '4th' => 0,
+#   '5th' => 0,
+#   'last' => 0
+# }
+
+# numbers_hash.each do |key, value|
+#   print "Enter the #{key} number: "
+#   numbers_hash[key] = gets.to_i
+# end
+
+# numbers_array = numbers_hash.values.slice(0, 5)
+
+# if numbers_array.include?(numbers_hash['last'])
+#   puts "The number #{numbers_hash['last']} appears in #{numbers_array}."
+# else
+#   puts "The number #{numbers_hash['last']} does not appear in #{numbers_array}."
+# end
