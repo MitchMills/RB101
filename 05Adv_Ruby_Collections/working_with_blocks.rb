2f5e732cb9284fb99aadba086909c30c4170970f
Mitch Mills <mitchmillsgcp@gmail.com>
2022-10-28 16:00:00 -0500

    Practiced working with blocks

---
+++ 05Adv_Ruby_Collections/working_with_blocks.rb
@@ -0,0 +1,27 @@
+# EXAMPLE 3
+array1 = [[1, 2], [3, 4]]
+
+array2 = array1.map do |arr|
+  puts arr.first
+  arr.first
+end
+
+p array1
+p array2
+
+
+# EXAMPLE 2
+# array1 = [[1, 2], [3, 4]]
+
+# array2 = array1.map do |arr|
+#   puts arr.first
+# end
+
+# p array1
+# p array2
+
+
+# EXAMPLE 1
+# [[1, 2], [3, 4]].each do |arr|
+#   puts arr.first
+# end
