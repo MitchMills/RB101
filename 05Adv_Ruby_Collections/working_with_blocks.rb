da2978bff2d275b9008106d8849268a9105e91a7
Mitch Mills <mitchmillsgcp@gmail.com>
2022-10-30 11:17:15 -0500

    Practiced working with blocks

--- 05Adv_Ruby_Collections/working_with_blocks.rb
+++ 05Adv_Ruby_Collections/working_with_blocks.rb
@@ -1,4 +1,113 @@
+# MUTATING COLLECTIONS WHILE ITERATING
+def remove_evens!(arr)
+  cloned_arr = arr.dup
+  cloned_arr.each do |num|
+    if num % 2 == 0
+      arr.delete(num)
+    end
+  end
+  arr
+end
+
+p array1 = [1, 1, 2, 3, 4, 6, 8, 9]
+array2 = remove_evens!(array1)
+p array1
+p array2
+
+
+
+# EXAMPLE 10
+# array1 = [
+#   [[1, 2], [3, 4]],
+#   [5, 6]
+# ]
+
+# array2 = 
+# array1.map do |arr|
+#   arr.map do |el|
+#     if el.to_s.size == 1  # it's an integer
+#       el + 1
+#     else                  # it's an array
+#       el.map do |n|
+#         n + 1
+#       end
+#     end
+#   end
+# end
+
+# p array1
+# p array2
+
+
+
+# EXAMPLE 9
+# array1 = [
+#   [[1], [2], [3], [4]],
+#   [['a'], ['b'], ['c']]
+# ]
+
+# array2 =
+# array1.map do |element1|
+#   element1.each do |element2|
+#     element2.partition do |element3|
+#       element3.size > 0
+#     end
+#   end
+# end
+
+# p array1
+# p array2
+
+
+# EXAMPLE 8
+# array1 = [[8, 13, 27], ['apple', 'banana', 'cantaloupe']]
+
+# array2 = []
+# array1.each do |arr|
+#   selected =  arr.select do |item|
+#                 if item.to_s.to_i == item
+#                   item > 13
+#                 else
+#                   item.size < 6
+#                 end
+#               end
+#   array2 << selected
+# end
+
+# array2 =
+# array1.map do |arr|
+#   arr.select do |item|
+#     if item.to_s.to_i == item # i.e. is it an integer?
+#       item > 13
+#     else
+#       item.size < 6
+#     end
+#   end
+# end
+
+# p array1
+# p array2
+
+
 # EXAMPLE 7
+# arr = [
+#   ['1',  '8', '11'], 
+#   ['2',  '6', '13'], 
+#   ['2', '12', '15'], 
+#   ['1',  '8',  '9']
+# ]
+
+# string_sort = arr.sort
+
+# p string_sort # --> [["1", "8", "11"], ["1", "8", "9"], ["2", "12", "15"], ["2", "6", "13"]]
+
+# num_sort =  arr.sort_by do |sub_arr|
+#               sub_arr.map do |num|
+#                 num.to_i
+#               end
+#             end
+
+# p num_sort # --> [["1", "8", "9"], ["1", "8", "11"], ["2", "6", "13"], ["2", "12", "15"]]
 
 
 # EXAMPLE 6
