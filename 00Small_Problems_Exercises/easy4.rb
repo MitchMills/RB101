31d45fccce6b7b69c7ecae3af444fe821b6b966b
Mitch Mills <mitchmillsgcp@gmail.com>
2022-11-08 16:19:56 -0600

    Worked on Easy 4 Small Problems

--- 00Small_Problems_Exercises/easy4.rb
+++ 00Small_Problems_Exercises/easy4.rb
@@ -1,15 +1,30 @@
-# 6 RUNNING TOTALS
-def running_total(array)
-  sum = 0
-  array.map do |num|
-    
-  end
-end
+require 'pry'
 
-p running_total([2, 5, 13]) #== [2, 7, 20]
-p running_total([14, 11, 7, 15, 20]) #== [14, 25, 32, 47, 67]
-p running_total([3]) #== [3]
-p running_total([]) #== []
+# 7 
+
+
+# 6 RUNNING TOTALS
+# def running_total(array)
+#   sum = 0
+#   new_array = []
+#   array.each_with_index do |num, index|
+#     new_array[index] = (num + sum)
+#     sum += num
+#   end
+#   new_array
+# end
+
+# def running_total(array)
+#   sum = 0
+#   array.map do |num|
+#     sum += num
+#   end
+# end
+
+# p running_total([2, 5, 13]) == [2, 7, 20]
+# p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+# p running_total([3]) == [3]
+# p running_total([]) == []
 
 
 # 5 MULTIPLES OF 3 AND 5
