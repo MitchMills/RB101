33aa5289dbd388ce23dc1e96cd0a35df37a8e503
Mitch Mills <mitchmillsgcp@gmail.com>
2022-11-13 09:18:39 -0600

    Recreated tic tac toe program from memory

--- 06Slghtly_Lrgr_Prgrms/sandbox.rb
+++ 06Slghtly_Lrgr_Prgrms/sandbox.rb
@@ -1,35 +1,28 @@
 INITIAL_MARK = ' '
 PLAYER_MARK = 'X'
 COMPUTER_MARK = 'O'
+WINNING_LINES = [
+  [1, 2, 3], [4, 5, 6], [7, 8, 9],  # rows
+  [1, 4, 7], [2, 5, 8], [3, 6, 9],  # columns
+  [1, 5, 9], [3, 5, 7]              # diagonals
+]
 
-def display_board(brd)
-  system('clear')
-  puts ""
-  puts "You're #{PLAYER_MARK}. Computer is #{COMPUTER_MARK}."
-  puts ""
-  puts "       |       |"
-  puts "   #{brd[1][0]}   |   #{brd[2][0]}   |   #{brd[3][0]}"
-  puts "      #{brd[1][1]}|      #{brd[2][1]}|      #{brd[3][1]}"
-  puts "-------+-------+-------"
-  puts "       |       |"
-  puts "   #{brd[4][0]}   |   #{brd[5][0]}   |   #{brd[6][0]}"
-  puts "      #{brd[4][1]}|      #{brd[5][1]}|      #{brd[6][1]}"
-  puts "-------+-------+-------"
-  puts "       |       |"
-  puts "   #{brd[7][0]}   |   #{brd[8][0]}   |   #{brd[9][0]}"
-  puts "      #{brd[7][1]}|      #{brd[8][1]}|      #{brd[9][1]}"
-  puts ""
+board = 
+{
+  1=>["1", "X"], 2=>["2", "X"], 3=>["3", "X"], 
+
+  4=>["4", " "], 5=>["5", " "], 6=>["6", " "], 
+
+  7=>["7", " "], 8=>["8", " "], 9=>["9", " "]
+}
+
+WINNING_LINES.each do |line|
+  if board.values_at(*line).flatten.count(PLAYER_MARK) == 3
+    puts 'Player'
+  end
 end
 
-def initialize_board
-  new_board = {}
-  (1..9).each { |num| new_board[num] = [INITIAL_MARK, "#{num}"] }
-  new_board
-end
-
-
-board = initialize_board()
-# board[5] = [PLAYER_MARK, ' ']
-# board[9] = [COMPUTER_MARK, ' ']
-# board[1] = [PLAYER_MARK, ' ']
-display_board(board)
+# line = [1, 2, 3]
+# p board.values_at(*line)
+# puts
+# p WINNING_LINES.flatten.count(1)
--- 06Slghtly_Lrgr_Prgrms/tictactoe2.rb
+++ 06Slghtly_Lrgr_Prgrms/tictactoe2.rb
@@ -1,6 +1,11 @@
 INITIAL_MARK = ' '
 PLAYER_MARK = 'X'
 COMPUTER_MARK = 'O'
+WINNING_LINES = [
+  [1, 2, 3], [4, 5, 6], [7, 8, 9],  # rows
+  [1, 4, 7], [2, 5, 8], [3, 6, 9],  # columns
+  [1, 5, 9], [3, 5, 7]              # diagonals
+]
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -8,10 +13,11 @@ end
 
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = ["#{num}", INITIAL_MARK]}
+  (1..9).each { |num| new_board[num] = [num, INITIAL_MARK]}
   new_board
 end
 
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 def display_board(brd)
   system('clear')
   puts
@@ -30,6 +36,7 @@ def display_board(brd)
   puts "      #{brd[7][0]}|      #{brd[8][0]}|      #{brd[9][0]}"
   puts
 end
+# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
 def empty_squares(brd)
   brd.keys.select { |num| brd[num][1] == INITIAL_MARK }
@@ -46,11 +53,58 @@ def player_picks_square!(brd)
   brd[square] = [' ', PLAYER_MARK]
 end
 
+def computer_picks_square!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = [' ', COMPUTER_MARK]
+end
 
-board = initialize_board()
-display_board(board)
-player_picks_square!(board)
-display_board(board)
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).flatten.count(PLAYER_MARK) == 3
+      return 'Player'
+    elsif brd.values_at(*line).flatten.count(COMPUTER_MARK) == 3
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+def winner?(brd)
+  !!detect_winner(brd)
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+# main game loop
+loop do
+  board = initialize_board()
+
+  loop do
+    display_board(board)
+
+    player_picks_square!(board)
+    break if winner?(board) || board_full?(board)
+
+    computer_picks_square!(board)
+    break if winner?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+  if winner?(board)
+    prompt("#{detect_winner(board)} won!")
+  else
+    prompt("It's a tie!")
+  end
+
+  prompt("Play again? (y or n)")
+  answer = gets.chomp.downcase
+  break unless answer.start_with?('y')
+end
+
+prompt("Thank you for playing Tic Tac Toe! Goodbye.")
 
 
 
@@ -62,9 +116,9 @@ display_board(board)
    4=>["4", " "], 5=>["5", " "], 6=>["6", " "], 
    7=>["7", " "], 8=>["8", " "], 9=>["9", " "]}
   player turn
-computer turn
-display board
-winner?
+  computer turn
+  display board
+  winner?
 board full?
 repeat
 play again?
