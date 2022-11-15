c7145ff003f04a1b20cd89645f94cee9204e1997
Mitch Mills <mitchmillsgcp@gmail.com>
2022-11-15 08:47:24 -0600

    Finished tic tac toe Match feature

--- 06Slghtly_Lrgr_Prgrms/tictactoe2.rb
+++ 06Slghtly_Lrgr_Prgrms/tictactoe2.rb
@@ -102,8 +102,8 @@ def update_score(score, winner)
 end
 
 def display_score(score)
-  prompt("SCORES: Player: #{score[:player_score]}, Computer: #{score[:computer_score]}, \
-Ties: #{score[:ties]}")
+  prompt("SCORES: Player: #{score[:player_score]}, \
+Computer: #{score[:computer_score]}, Ties: #{score[:ties]}")
 end
 
 def detect_match_winner(score)
@@ -124,77 +124,70 @@ end
 def display_match_winner(match_winner)
   case match_winner
   when 'Player'
-    prompt("You are the Match Winner!")
+    prompt("You have won the Match!")
   when 'Computer'
-    prompt("Computer is the Match Winner!")
+    prompt("Computer has won the Match!")
   when 'Tie'
-    prompt("The Match has ended in a tie.")
+    prompt("This Match has ended in a tie.")
   end
 end
-
-
 
 # main game loop
 first_time = true
 
-loop do ### PROGRAM LOOP BEGIN
+loop do ### MATCH LOOP BEGIN
   system('clear')
   score = { player_score: 0, computer_score: 0, ties: 0 }
-  
-  if first_time
-    prompt("Welcome to Tic Tac Toe. You will play against the computer.")
+  if first_time == true
+    prompt("Welcome to Tic Tac Toe! You will play against the computer.")
     first_time = false
   else
-    prompt("Welcome back! Ready for another match?")
+    prompt("Welcome back! Ready for another Match?")
   end
-  prompt("Whoever wins the most games out of five will be the Match Winner.")
+  prompt("The player who wins the most games out of five will win the Match.")
+  prompt("Enter any key to continue.")
+  gets
 
-
-  loop do ### MATCH LOOP BEGIN
-
+  loop do ### GAME LOOP BEGIN
+    board = initialize_board()
     prompt("Let's begin Game #{score.values.sum + 1}.")
-    prompt("Enter any key to continue.")
-    gets
 
-    loop do ### GAME LOOP BEGIN
-      board = initialize_board()
-
-      loop do
-        display_board(board, score)
-
-        player_picks_square!(board)
-        break if game_winner?(board) || board_full?(board)
-
-        computer_picks_square!(board)
-        break if game_winner?(board) || board_full?(board)
-      end
-
+    loop do ### MOVES LOOP BEGIN
       display_board(board, score)
-      
-      
-    
 
-      game_winner = detect_game_winner(board)
-      if game_winner?(board)
-        prompt("#{game_winner} won!")
-      else
-        prompt("It's a tie!")
-      end
+      player_picks_square!(board)
+      break if game_winner?(board) || board_full?(board)
 
-      update_score(score, game_winner)
-      display_score(score)
-      prompt("Enter any key to continue to Game #{score.values.sum + 1}.")
-      gets
-      
+      computer_picks_square!(board)
+      break if game_winner?(board) || board_full?(board)
+    end ### MOVES LOOP END
 
-      break if match_winner?(score)
+    display_board(board, score)
 
-    end ### GAME LOOP END
-    match_winner = detect_match_winner(score)
-    display_match_winner(match_winner)
-    
-  end ### MATCH LOOP END
+    game_winner = detect_game_winner(board)
+    if game_winner?(board)
+      prompt("#{game_winner} won this game!")
+    else
+      prompt("It's a tie!")
+    end
+    puts
 
-end ### PROGRAM LOOP END
+    update_score(score, game_winner)
+    display_score(score)
+    break if match_winner?(score)
+
+    prompt("Enter any key to continue to Game #{score.values.sum + 1}.")
+    gets
+  end ### GAME LOOP END
+
+  match_winner = detect_match_winner(score)
+  display_match_winner(match_winner)
+  puts
+
+  prompt("If you would you like to play another Match, enter 'y'.")
+  prompt("Enter any other key to exit.")
+  another_match = gets.chomp.downcase
+  break unless another_match == 'y'
+end ### MATCH LOOP END
 
 prompt("Thank you for playing Tic Tac Toe! Goodbye.")
