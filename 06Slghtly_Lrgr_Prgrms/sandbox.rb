9f43b149554a184e07d5eaaaf90f6e0d646a31e3
Mitch Mills <mitchmillsgcp@gmail.com>
2022-11-25 09:32:55 -0600

    Worked out a few bugs in tic tac toe

--- 06Slghtly_Lrgr_Prgrms/sandbox.rb
+++ 06Slghtly_Lrgr_Prgrms/sandbox.rb
@@ -1,43 +1,68 @@
 INITIAL_MARK = ' '
 PLAYER_MARK = 'X'
 COMPUTER_MARK = 'O'
+ALL_SQUARES = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+MIDDLE_SQUARE = 5
+CORNER_SQUARES = [1, 3, 7, 9]
 WINNING_LINES = [
   [1, 2, 3], [4, 5, 6], [7, 8, 9],  # rows
   [1, 4, 7], [2, 5, 8], [3, 6, 9],  # columns
   [1, 5, 9], [3, 5, 7]              # diagonals
 ]
 
-def prompt(msg)
-  puts "=> #{msg}"
+def game_number(score)
+  score.values.sum + 1
 end
 
-def detect_game_winner(brd)
-  WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(PLAYER_MARK) == 3
-      return :player
-    elsif brd.values_at(*line).count(COMPUTER_MARK) == 3
-      return :computer
-    end
+def ahead?(score, player1, player2)
+  score[player1] > score[player2]
+end
+
+def detect_match_winner(score, current_player)
+  if (score[current_player] >= 3) || 
+    (game_number(score) > 5 && ahead?(score, current_player, alternate_player(current_player)))
+    return current_player
+
+
+    
+  elsif (score[player2] >= 3) || 
+    (game_number(score) > 5 && ahead?(score, player2, player1))
+    return player2
+  elsif
+    game_number(score) > 5
+    return :tie
   end
   nil
 end
 
-def game_winner?(brd)
-  !!detect_game_winner(brd)
+def match_winner?(score, player1, player2)
+  !!detect_match_winner(score, player1, player2)
 end
 
-def update_score(brd, score, winner)
-  if game_winner?(brd)
-    score[winner] += 1
-  else
-    score[:ties] += 1
+def display_match_winner(match_winner)
+  case match_winner
+  when :player
+    prompt("You have won the Match!")
+  when :computer
+    prompt("Computer has won the Match!")
+  when :tie
+    prompt("This Match has ended in a tie.")
   end
-  score
 end
 
-board = {1=>"X", 2=>"X", 3=>"X", 4=>" ", 5=>" ", 6=>" ", 7=>" ", 8=>" ", 9=>" "}
-score = { player: 0, computer: 0, ties: 0 }
-game_winner = detect_game_winner(board)
-update_score(board, score, game_winner)
-p game_winner
-p score
+def alternate_player(current_player)
+  if current_player == :computer
+    :player
+  else
+    :computer
+  end
+end
+
+
+current_player = :player
+score = { player: 1, computer: 2, ties: 2 }
+
+match_winner = detect_match_winner(score, current_player, alternate_player(current_player))
+match_winner2 = detect_match_winner(score, :player, :computer)
+p match_winner
+p match_winner2
--- 06Slghtly_Lrgr_Prgrms/tictactoe2.rb
+++ 06Slghtly_Lrgr_Prgrms/tictactoe2.rb
@@ -3,14 +3,30 @@ require 'pry-byebug'
 INITIAL_MARK = ' '
 PLAYER_MARK = 'X'
 COMPUTER_MARK = 'O'
+ALL_SQUARES = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+MIDDLE_SQUARE = 5
+CORNER_SQUARES = [1, 3, 7, 9]
 WINNING_LINES = [
   [1, 2, 3], [4, 5, 6], [7, 8, 9],  # rows
   [1, 4, 7], [2, 5, 8], [3, 6, 9],  # columns
   [1, 5, 9], [3, 5, 7]              # diagonals
 ]
 
+
+
 def prompt(msg)
   puts "=> #{msg}"
+end
+
+def intro(first_time)
+  if first_time == true
+    prompt("Welcome to Tic Tac Toe! You will play against the computer.")
+  else
+    prompt("Welcome back! Ready for another Match?")
+  end
+
+  prompt("Whoever wins the most games out of five will win the Match.")
+  puts
 end
 
 def choose_first_player
@@ -28,16 +44,23 @@ def choose_first_player
 end
 
 def first_player
-  first_player =  case choose_first_player
+  first_player =  case choose_first_player()
                   when 1 then :player
                   when 2 then :computer
                   when 3 then [:player, :computer].sample
                   end
 end
 
+def start_match(current_player)
+  prompt("#{current_player.capitalize} will go first on Game 1.")
+  prompt("After that the first player will alternate.")
+  prompt("Enter any key to continue.")
+  gets
+end
+
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = INITIAL_MARK }
+  ALL_SQUARES.each { |num| new_board[num] = INITIAL_MARK }
   new_board
 end
 
@@ -70,13 +93,12 @@ def empty_squares(brd)
 end
 
 def empty_corners(brd)
-  corners = [1, 3, 7, 9]
-  corners.intersection(empty_squares(brd))
+  CORNER_SQUARES.intersection(empty_squares(brd))
 end
 
 def board_numbers(brd)
   brd_nums = {}
-  (1..9).each do |num|
+  ALL_SQUARES.each do |num|
     brd_nums[num] = if empty_squares(brd).include?(num)
                       num
                     else
@@ -115,7 +137,7 @@ end
 def get_target_square(line, brd, mark)
   if (brd.values_at(*line).count(mark) == 2) &&
      (brd.values_at(*line).count(INITIAL_MARK) == 1)
-    line.intersection(empty_squares(brd)).first
+    return line.intersection(empty_squares(brd)).first
   end
   nil
 end
@@ -146,8 +168,8 @@ def computer_picks_square!(brd)
                     all_targets[:opportunities].sample
                   elsif all_targets[:threats].size > 0
                     all_targets[:threats].sample
-                  elsif empty_squares(brd).include?(5)
-                    5
+                  elsif empty_squares(brd).include?(MIDDLE_SQUARE)
+                    MIDDLE_SQUARE
                   elsif empty_corners(brd).size > 0
                     empty_corners(brd).sample
                   else
@@ -171,6 +193,15 @@ end
 
 def game_winner?(brd)
   !!detect_game_winner(brd)
+end
+
+def announce_game_winner(brd, game_winner)
+  if game_winner?(brd)
+    prompt("#{game_winner.capitalize} won this game!")
+  else
+    prompt("It's a tie!")
+  end
+  puts
 end
 
 def board_full?(brd)
@@ -237,65 +268,57 @@ def alternate_player(current_player)
   end
 end
 
+def outro
+  prompt("If you would you like to play another Match, enter 'y'.")
+  prompt("Enter any other key to exit.")
+  another_match = gets.chomp.downcase
+end
+
+
+
+
 # main game loop
 first_time = true
 
-loop do ### MATCH LOOP BEGIN
+loop do # Match Loop Begin
   system('clear')
   score = { player: 0, computer: 0, ties: 0 }
 
-  if first_time == true
-    prompt("Welcome to Tic Tac Toe! You will play against the computer.")
-    first_time = false
-  else
-    prompt("Welcome back! Ready for another Match?")
-  end
-
-  prompt("Whoever wins the most games out of five will win the Match.")
-  puts
+  intro(first_time)
+  first_time = false
 
   current_player = first_player()
-  prompt("#{current_player.capitalize} will go first on Game 1.")
-  prompt("After that the first player will alternate.")
-  prompt("Enter any key to continue.")
-  gets
+  start_match(current_player)
 
-  loop do ### SINGLE GAME LOOP BEGIN
+  loop do # Single Game Loop Begin
     board = initialize_board
 
-    loop do ### MOVES LOOP BEGIN
+    loop do # Moves Loop Begin
       display_board(board, score) if current_player == :player
       pick_square!(board, current_player)
       current_player = alternate_player(current_player)
       break if game_winner?(board) || board_full?(board)
-    end ### MOVES LOOP END
+    end # Moves Loop End
 
     display_board(board, score)
 
     game_winner = detect_game_winner(board)
-    if game_winner?(board)
-      prompt("#{game_winner.capitalize} won this game!")
-    else
-      prompt("It's a tie!")
-    end
-    puts
-
+    announce_game_winner(board, game_winner)
+    
     update_score(board, score, game_winner)
     display_score(score)
     break if match_winner?(score, current_player, alternate_player(current_player))
 
     prompt("Enter any key to continue to Game #{score.values.sum + 1}.")
     gets
-  end ### SINGLE GAME LOOP END
+  end # Single Game Loop end
 
   match_winner = detect_match_winner(score, current_player, alternate_player(current_player))
   display_match_winner(match_winner)
   puts
 
-  prompt("If you would you like to play another Match, enter 'y'.")
-  prompt("Enter any other key to exit.")
-  another_match = gets.chomp.downcase
+  another_match = outro()
   break unless another_match == 'y'
-end ### MATCH LOOP END
+end # Match Loop End
 
 prompt("Thank you for playing Tic Tac Toe! Goodbye.")
