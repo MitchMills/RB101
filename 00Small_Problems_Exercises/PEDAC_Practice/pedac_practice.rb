fcdab4bd273b63a002e6389aacfd83cace8e76c9
Mitch Mills <mitchmillsgcp@gmail.com>
2022-11-29 16:56:52 -0600

    Worked on PEDAC examples

--- 00Small_Problems_Exercises/PEDAC_Practice/pedac_practice.rb
+++ 00Small_Problems_Exercises/PEDAC_Practice/pedac_practice.rb
@@ -5,33 +5,31 @@
 # array2 = %w(h a c k e r r a n k)
 
 def anagram_difference(string1, string2)
-  p "----------"
-  array1 = string1.chars
-  array2 = string2.chars
-  both_arrays = array1 + array2
-  
-  not_in_common = []
-  not_in_common << array1.reject do |char| 
-    array2.include?(char)
+  if string1.size <= string2.size
+    short_array = string1.chars
+    long_array = string2.chars
+  else
+    short_array = string2.chars
+    long_array = string1.chars
   end
-  p "NIC1: #{not_in_common}"
 
-  not_in_common << array2.reject do |char| 
-    array1.include?(char)
+  in_common = []
+  short_array.each do |char|
+    in_common << char if long_array.include?(char)
   end
-  p "NIC2: #{not_in_common}"
-  
+
+  difference = (short_array.size + long_array.size) - (in_common.size * 2)
 end
 
-# p anagram_difference('', '') == 0
-# p anagram_difference('a', '') == 1
-# p anagram_difference('', 'a') == 1
-# p anagram_difference('ab', 'a') == 1
-# p anagram_difference('ab', 'ba') == 0
-# p anagram_difference('ab', 'cd') == 4
-anagram_difference('aab', 'a') #== 2
-anagram_difference('a', 'aab') #== 2
-# p anagram_difference('codewars', 'hackerrank') == 10
-# p anagram_difference('abcdeee', 'fghieee') #== 8
-anagram_difference('abcdeee', 'fghiee') #== 9
-
+p anagram_difference('', '') == 0
+p anagram_difference('a', '') == 1
+p anagram_difference('', 'a') == 1
+p anagram_difference('ab', 'a') == 1
+p anagram_difference('ab', 'ba') == 0
+p anagram_difference('ab', 'cd') == 4
+p anagram_difference('aab', 'a') == 2
+p anagram_difference('a', 'aab') == 2
+p anagram_difference('codewars', 'hackerrank') == 10
+p anagram_difference('abcdeee', 'fghieee') == 8
+p anagram_difference('abcdeee', 'fghiee') == 9
+p anagram_difference('abcdee', 'fghieee') == 9
