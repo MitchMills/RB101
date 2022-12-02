aaa572995d226d66dac8861d38d18d00a032474d
Mitch Mills <mitchmillsgcp@gmail.com>
2022-12-02 09:52:34 -0600

    Worked on general program structure

---
+++ 06Slghtly_Lrgr_Prgrms/TwentyOne/twenty_one.rb
@@ -0,0 +1,47 @@
+CARD_FACES = [
+  '2', '3', '4', '5', '6', '7', '8', '9', '10', 
+  'J', 'Q', 'K', 'A'
+]
+CARD_SUITS = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+
+# DECK = CARD_FACES.product(CARD_SUITS)
+
+DECK = [
+  ["2", "Clubs"], ["2", "Diamonds"], ["2", "Hearts"], ["2", "Spades"],
+  ["3", "Clubs"], ["3", "Diamonds"], ["3", "Hearts"], ["3", "Spades"],
+  ["4", "Clubs"], ["4", "Diamonds"], ["4", "Hearts"], ["4", "Spades"],
+  ["5", "Clubs"], ["5", "Diamonds"], ["5", "Hearts"], ["5", "Spades"],
+  ["6", "Clubs"], ["6", "Diamonds"], ["6", "Hearts"], ["6", "Spades"],
+  ["7", "Clubs"], ["7", "Diamonds"], ["7", "Hearts"], ["7", "Spades"],
+  ["8", "Clubs"], ["8", "Diamonds"], ["8", "Hearts"], ["8", "Spades"],
+  ["9", "Clubs"], ["9", "Diamonds"], ["9", "Hearts"], ["9", "Spades"],
+  ["10", "Clubs"], ["10", "Diamonds"], ["10", "Hearts"], ["10", "Spades"],
+  ["J", "Clubs"], ["J", "Diamonds"], ["J", "Hearts"], ["J", "Spades"],
+  ["Q", "Clubs"], ["Q", "Diamonds"], ["Q", "Hearts"], ["Q", "Spades"],
+  ["K", "Clubs"], ["K", "Diamonds"], ["K", "Hearts"], ["K", "Spades"],
+  ["A", "Clubs"], ["A", "Diamonds"], ["A", "Hearts"], ["A", "Spades"]
+]
+
+def welcome()
+  foo
+end
+
+def initialize_deck()
+  foo
+end
+
+def deal_cards()
+  foo
+end
+
+
+# main game loop
+# welcome()
+# initialize_deck()
+# deal_cards()
+# player_turn()
+# dealer_turn()
+# determine_winner()
+# outro()
+
+p DECK
