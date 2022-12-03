6f924ad34f152757adbf00c99a806ad921553e2b
Mitch Mills <mitchmillsgcp@gmail.com>
2022-12-03 08:28:59 -0600

    Worked on basic game structure

--- 06Slghtly_Lrgr_Prgrms/TwentyOne/twenty_one.rb
+++ 06Slghtly_Lrgr_Prgrms/TwentyOne/twenty_one.rb
@@ -4,38 +4,53 @@ CARD_FACES = [
 ]
 CARD_SUITS = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 
-# DECK = CARD_FACES.product(CARD_SUITS)
+# deck = [
+#   ["2", "Clubs"], ["2", "Diamonds"], ["2", "Hearts"], ["2", "Spades"],
+#   ["3", "Clubs"], ["3", "Diamonds"], ["3", "Hearts"], ["3", "Spades"],
+#   ["4", "Clubs"], ["4", "Diamonds"], ["4", "Hearts"], ["4", "Spades"],
+#   ["5", "Clubs"], ["5", "Diamonds"], ["5", "Hearts"], ["5", "Spades"],
+#   ["6", "Clubs"], ["6", "Diamonds"], ["6", "Hearts"], ["6", "Spades"],
+#   ["7", "Clubs"], ["7", "Diamonds"], ["7", "Hearts"], ["7", "Spades"],
+#   ["8", "Clubs"], ["8", "Diamonds"], ["8", "Hearts"], ["8", "Spades"],
+#   ["9", "Clubs"], ["9", "Diamonds"], ["9", "Hearts"], ["9", "Spades"],
+#   ["10", "Clubs"], ["10", "Diamonds"], ["10", "Hearts"], ["10", "Spades"],
+#   ["J", "Clubs"], ["J", "Diamonds"], ["J", "Hearts"], ["J", "Spades"],
+#   ["Q", "Clubs"], ["Q", "Diamonds"], ["Q", "Hearts"], ["Q", "Spades"],
+#   ["K", "Clubs"], ["K", "Diamonds"], ["K", "Hearts"], ["K", "Spades"],
+#   ["A", "Clubs"], ["A", "Diamonds"], ["A", "Hearts"], ["A", "Spades"]
+# ]
 
-DECK = [
-  ["2", "Clubs"], ["2", "Diamonds"], ["2", "Hearts"], ["2", "Spades"],
-  ["3", "Clubs"], ["3", "Diamonds"], ["3", "Hearts"], ["3", "Spades"],
-  ["4", "Clubs"], ["4", "Diamonds"], ["4", "Hearts"], ["4", "Spades"],
-  ["5", "Clubs"], ["5", "Diamonds"], ["5", "Hearts"], ["5", "Spades"],
-  ["6", "Clubs"], ["6", "Diamonds"], ["6", "Hearts"], ["6", "Spades"],
-  ["7", "Clubs"], ["7", "Diamonds"], ["7", "Hearts"], ["7", "Spades"],
-  ["8", "Clubs"], ["8", "Diamonds"], ["8", "Hearts"], ["8", "Spades"],
-  ["9", "Clubs"], ["9", "Diamonds"], ["9", "Hearts"], ["9", "Spades"],
-  ["10", "Clubs"], ["10", "Diamonds"], ["10", "Hearts"], ["10", "Spades"],
-  ["J", "Clubs"], ["J", "Diamonds"], ["J", "Hearts"], ["J", "Spades"],
-  ["Q", "Clubs"], ["Q", "Diamonds"], ["Q", "Hearts"], ["Q", "Spades"],
-  ["K", "Clubs"], ["K", "Diamonds"], ["K", "Hearts"], ["K", "Spades"],
-  ["A", "Clubs"], ["A", "Diamonds"], ["A", "Hearts"], ["A", "Spades"]
-]
+def prompt(message)
+  puts "=> #{message}"
+end
 
 def welcome()
-  foo
+  prompt "Welcome to Twenty-One!"
+  prompt "Enter any key to begin."
+  gets
 end
 
 def initialize_deck()
-  foo
+  CARD_FACES.product(CARD_SUITS)
 end
 
-def deal_cards()
-  foo
+def get_initial_hands(deck, hands)
+  hands = deal_cards(deck, hand, 2)
 end
 
+def deal_cards(deck, hand, number)
+  number.times { |i| hand << get_card(deck) }
+  hand
+end
 
-# main game loop
+def get_card(deck)
+  card = deck.sample
+  deck.delete(card)
+  p card
+  card
+end
+
+# MAIN GAME LOOP
 # welcome()
 # initialize_deck()
 # deal_cards()
@@ -44,4 +59,17 @@ end
 # determine_winner()
 # outro()
 
-p DECK
+loop do
+  welcome()
+  deck = initialize_deck()
+  hands = { player: [], dealer: [] }
+  get_initial_hands(deck, hands)
+  display_hands(player_hand, dealer_hand)
+end
+
+# deck = initialize_deck()
+# player_hand = []
+# player_hand = deal_cards(deck, player_hand, 2)
+# p player_hand
+# puts
+# p deck
