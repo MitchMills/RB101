06b549a3781d2dc66d7ab5b5ef37fdc53270fe50
Mitch Mills <mitchmillsgcp@gmail.com>
2022-12-05 13:42:48 -0600

    Worked on written exam exercises

--- 00Small_Problems_Exercises/PEDAC_Practice/pedac_practice.rb
+++ 00Small_Problems_Exercises/PEDAC_Practice/pedac_practice.rb
@@ -1,36 +1,14 @@
-# string1 = "codewars"
-# string2 = "hackerrank"
-
-# array1 = %w(c o d e w a r s)
-# array2 = %w(h a c k e r r a n k)
-# intersection = %w(c e a r)
-
-def anagram_difference(string1, string2)
-  if string1.size <= string2.size
-    short_array = string1.chars
-    long_array = string2.chars
-  else
-    short_array = string2.chars
-    long_array = string1.chars
-  end
-
-  in_common = []
-  short_array.each do |char|
-    in_common << char if long_array.include?(char)
-  end
-
-  difference = (short_array.size + long_array.size) - (in_common.size * 2)
+def spin_me(str)
+  str.split.each do |word|
+    word.reverse!
+  end.join(" ")
 end
 
-p anagram_difference('', '') == 0
-p anagram_difference('a', '') == 1
-p anagram_difference('', 'a') == 1
-p anagram_difference('ab', 'a') == 1
-p anagram_difference('ab', 'ba') == 0
-p anagram_difference('ab', 'cd') == 4
-p anagram_difference('aab', 'a') == 2
-p anagram_difference('a', 'aab') == 2
-p anagram_difference('codewars', 'hackerrank') == 10
-p anagram_difference('abcdeee', 'fghieee') == 8
-p anagram_difference('abcdeee', 'fghiee') == 9
-p anagram_difference('abcdee', 'fghieee') == 9
+p string = "hello world"
+p string.object_id
+puts
+p string2 = spin_me(string)
+p string2.object_id
+puts
+p string
+p string.object_id
--- 00Small_Problems_Exercises/sandbox.rb
+++ 00Small_Problems_Exercises/sandbox.rb
@@ -1,37 +1,7 @@
-def minSubLength(num_arr, min_sum)
-  if num_arr.sum < min_sum
-    return 0
-  elsif num_arr.max >= min_sum
-    return 1
-  end
-
-  chunk_size = 2
-  loop do
-    start_index = 0
-    loop do
-      if num_arr[start_index, chunk_size].sum >= min_sum
-        return chunk_size
-      else
-        start_index += 1
-      end
-
-      break if start_index > (num_arr.size - chunk_size)
-    end
-    chunk_size += 1
-    
-    if chunk_size >= num_arr.size
-      if num_arr.sum >= min_sum
-        return chunk_size
-      else
-        return 0
-      end
-    end
-
-  end      
+def spin_me(str)
+  str.split.each do |word|
+    word.reverse!
+  end.join(" ")
 end
 
-p minSubLength([2, 3, 1, 2, 4, 3], 7) == 2
-p minSubLength([1, 10, 5, 2, 7], 9) == 1
-p minSubLength([1, 11, 100, 1, 0, 200, 3, 2, 1, 250], 280) == 4
-p minSubLength([1, 2, 4], 8) == 0
-p minSubLength([1, 1, 1, 1, 1], 5) == 5
+p spin_me("hello world")
