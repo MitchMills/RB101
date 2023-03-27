a12917e0a84be10be3459051bb337d7d079c27cd
Mitch Mills <mitchmillsgcp@gmail.com>
2023-03-27 09:54:28 -0500

    Reworked docs exercises

--- 00Small_Problems_Exercises/documentation_again.rb
+++ 00Small_Problems_Exercises/documentation_again.rb
@@ -1,32 +1,37 @@
-
-# KEYWORD ARGUMENTS
-# 5.step(by: 3, to: 10) { |value| puts value } # --> 5 8
+### 7 PARENT CLASS
 
 
-# MULTIPLE SIGNATURES
-# array = %w(a b c d e)
-# # puts array.fetch(7) # --> IndexError
-# puts array.fetch(7, 'beats me') # --> 'beats me'
-# puts array.fetch(7) { |index| index**2 } # --> 49
+### 6 KEYWORD ARGUMENTS
+# 5.step(to: 10, by: 3) { |value| puts value }
 
+### 5 MULTIPLE SIGNATURES
+# a = %w(a b c d e)
+# p a[7]
+# # puts a.fetch(7)
+# puts a.fetch(7, 'beats me')
+# puts a.fetch(7) { |index| index**2 }
 
-# MANDATORY BLOCKS
-# array = [1, 4, 8, 11, 15, 19]
-# found_num = array.bsearch { |num| num > 8 }
-# p found_num
+### 4 MANDATORY BLOCKS
+# a = [1, 4, 8, 11, 15, 19]
+# p a.bsearch {|num| num > 8}
 
-# DEFAULT ARGUMENTS IN THE MIDDLE
+### 3 DEFAULT ARGUMENTS IN THE MIDDLE
 # def my_method(a, b = 2, c = 3, d)
 #   p [a, b, c, d]
 # end
 
-# my_method(4, 5, 6) # --> [4, 5, 3, 6]
+# my_method(4, 5, 6)
 
-# OPTIONAL ARGUMENTS REDUX
+### 2 OPTIONAL ARGUMENTS REDUX
 # require 'date'
 
-# puts Date.civil # --> -4712-01-01
-# puts Date.civil(2016) # --> 2016-01-01
-# puts Date.civil(2016, 5) # --> 2016-05-01
-# puts Date.civil(2016, 5, 13) # --> 2016-05-13
-# puts Date.civil(1751, 5, 13, Date::ENGLAND)
+# puts Date.civil
+# puts Date.civil(2016)
+# puts Date.civil(2016, 5)
+# puts Date.civil(2016, 5, 13)
+
+### 1 CLASS AND INSTANCE METHODS
+# puts File.path('bin')
+
+# f = File.new('my-file.txt')
+# puts f.path
