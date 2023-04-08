ba83d4c8f7a8784e4603e5f44d5db4c307c4ab3e
Mitch Mills <mitchmillsgcp@gmail.com>
2023-04-08 08:20:02 -0500

    Worked on easy4 exercises

--- 00Small_Problems_Exercises/easy4.rb
+++ 00Small_Problems_Exercises/easy4.rb
@@ -1,156 +1,36 @@
-require 'pry'
+### 2 WHAT CENTURY IS THAT?
+def century(year)
+  modifier = (year % 100 == 0) ? 0 : 1
+  century = ((year / 100) + modifier).to_s
+  suffix =  case century[-1]
+            when '1' then 'st'
+            when '2' then 'nd'
+            when '3' then 'rd'
+            else 'th'
+            end
+  if (century[-2, 2].to_i > 10) && (century[-2, 2].to_i < 14)
+    suffix = 'th'
+  end
+  century + suffix
+end
 
-# 7 
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
 
-
-# 6 RUNNING TOTALS
-# def running_total(array)
-#   sum = 0
-#   new_array = []
-#   array.each_with_index do |num, index|
-#     new_array[index] = (num + sum)
-#     sum += num
-#   end
-#   new_array
+### 1 SHORT LONG SHORT
+# def short_long_short(str1, str2)
+#   short =  (str1.size < str2.size) ? str1 : str2
+#   long =   (str1.size > str2.size) ? str1 : str2
+#   short + long + short 
 # end
 
-# def running_total(array)
-#   sum = 0
-#   array.map do |num|
-#     sum += num
-#   end
-# end
-
-# p running_total([2, 5, 13]) == [2, 7, 20]
-# p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
-# p running_total([3]) == [3]
-# p running_total([]) == []
-
-
-# 5 MULTIPLES OF 3 AND 5
-# def multisum(number)
-#   multiples = (1..number).select do |num|
-#     num % 3 == 0 || num % 5 == 0
-#   end
-#   multiples.sum
-# end
-
-# # def multisum(num)
-# #   multiples = []
-# #   (1..num).each do |num|
-# #     if num % 3 == 0 || num % 5 == 0
-# #       multiples << num
-# #     end
-# #   end
-# #   multiples.sum
-# # end
-
-# p multisum(3) == 3
-# p multisum(5) == 8
-# p multisum(10) == 33
-# p multisum(1000) == 234168
-
-
-# 4 LEAP YEARS II
-# def leap_year?(year)
-#   if (year % 100 == 0) && (year > 1752)
-#     year % 400 == 0
-#   else
-#     year % 4 == 0
-#   end
-# end
-
-# # def leap_year?(year)
-# #   if year < 1752
-# #     year % 4 == 0
-# #   else
-# #     (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))
-# #   end
-# # end
-
-# p leap_year?(2016) == true
-# p leap_year?(2015) == false
-# p leap_year?(2100) == false
-# p leap_year?(2400) == true
-# p leap_year?(240000) == true
-# p leap_year?(240001) == false
-# p leap_year?(2000) == true
-# p leap_year?(1900) == false
-# p leap_year?(1752) == true
-# p leap_year?(1700) == true
-# p leap_year?(1) == false
-# p leap_year?(100) == true
-# p leap_year?(400) == true
-
-
-# 3 LEAP YEARS I
-# def leap_year?(year)
-#   (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))
-# end
-
-# def leap_year?(year)
-#   if (year % 4 == 0) && (year % 100 != 0)
-#     true
-#   else 
-#     year % 400 == 0
-#   end
-# end
-
-# p leap_year?(2016) == true
-# p leap_year?(2015) == false
-# p leap_year?(2100) == false
-# p leap_year?(2400) == true
-# p leap_year?(240000) == true
-# puts
-# p leap_year?(240001) == false
-# p leap_year?(2000) == true
-# p leap_year?(1900) == false
-# p leap_year?(1752) == true
-# p leap_year?(1700) == false
-# puts
-# p leap_year?(1) == false
-# p leap_year?(100) == false
-# p leap_year?(400) == true
-
-
-# 2 WHAT CENTURY?
-# def century(year)
-#   increment = (year % 100) == 0 ? 0 : 1
-#   century = (year / 100) + increment
-#   century.to_s + get_suffix(century)
-# end
-
-# def get_suffix(century)
-#   return 'th' if (11..13).include?(century % 100)
-
-#   case century % 10
-#   when 1 then 'st'
-#   when 2 then 'nd'
-#   when 3 then 'rd'
-#   else 'th'
-#   end
-
-# end
-
-# p century(2000) #== '20th'
-# p century(2001) #== '21st'
-# p century(1965) #== '20th'
-# p century(256) #== '3rd'
-# p century(5) #== '1st'
-# p century(10103) #== '102nd'
-# p century(1052) #== '11th'
-# p century(1127) #== '12th'
-# p century(11201) #== '113th'
-
-
-# 1 SHORT LONG SHORT
-# def short_long_short(string1, string2)
-#   if string1.size > string2.size
-#     string2 + string1 + string2
-#   else
-#     string1 + string2 + string1
-#   end
-# end
 
 # p short_long_short('abc', 'defgh') == "abcdefghabc"
 # p short_long_short('abcde', 'fgh') == "fghabcdefgh"
--- 00Small_Problems_Exercises/pedac_scratchpad.md
+++ 00Small_Problems_Exercises/pedac_scratchpad.md
@@ -1,26 +1,51 @@
 # PROBLEM
+Write a method that takes a year as input and returns the century. The return value should be a string that begins with the century number, and ends with st, nd, rd, or th as appropriate for that number.
 
+New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
 
-  Input: 
-  Output:
+  Input: integer
+  Output: string
 
   ## Rules
   ### explicit
+  - input is an integer representing a year
+  - output is a string representing the century
+    - begins with century number
+    - ends with appropriate suffix ('st', 'nd', 'rd', or 'th')
+  - new centuries begin in years that end with 01
+    - e.g. 1900 = 19th century, 1901 = 20th century
   
   ### implicit
-
+  - Assume the year is AD / CE
 
   ## Questions
 
 
 # EXAMPLES
-
+century(2000) == '20th'
+century(2001) == '21st'
+century(1965) == '20th'
+century(256) == '3rd'
+century(5) == '1st'
+century(10103) == '102nd'
+century(1052) == '11th'
+century(1127) == '12th'
+century(11201) == '113th'
 
 # DATA STRUCTURES
 
 
 # ALGORITHM
- 
+- divide input integer by 100
+  - if modulus == 0, then `century` = (quotient + 1)
+  - else `century` = quotient
+- convert `century` to a string
+  - get last character to determine `suffix`
+    - if '1' => 'st'
+    - if '2' => 'nd'
+    - if '3' => 'rd'
+    - else => 'th'
+- return `century` + `suffix`
 
 
 # # # # # # # # #
