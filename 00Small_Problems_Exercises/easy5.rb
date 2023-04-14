28e50cbd801f6509d7ef09ddbe80ecb9cc9efa80
Mitch Mills <mitchmillsgcp@gmail.com>
2023-04-14 15:39:18 -0500

    Worked on easy5 exercises

--- 00Small_Problems_Exercises/easy5.rb
+++ 00Small_Problems_Exercises/easy5.rb
@@ -1,15 +1,25 @@
 ### 2 AFTER MIDNIGHT I
-def time_of_day(num)
+MINUTES_PER_HOUR = 60
+MINUTES_PER_DAY = 1440
 
+def prepend_zero(int)
+  int < 10 ? int.to_s.prepend('0') : int.to_s
 end
 
-time_of_day(0) == "00:00"
-time_of_day(-3) == "23:57"
-time_of_day(35) == "00:35"
-time_of_day(-1437) == "00:03"
-time_of_day(3000) == "02:00"
-time_of_day(800) == "13:20"
-time_of_day(-4231) == "01:29"
+def time_of_day(num)
+  normalized_minutes = num % MINUTES_PER_DAY
+  hrs_and_mins = normalized_minutes.divmod(MINUTES_PER_HOUR)
+  hrs_and_mins.map! { |ele| prepend_zero(ele) }
+  "#{hrs_and_mins[0]}:#{hrs_and_mins[1]}"
+end
+
+p time_of_day(0) == "00:00"
+p time_of_day(-3) == "23:57"
+p time_of_day(35) == "00:35"
+p time_of_day(-1437) == "00:03"
+p time_of_day(3000) == "02:00"
+p time_of_day(800) == "13:20"
+p time_of_day(-4231) == "01:29"
 
 ### 1 ASCII STRING VALUE
 # def ascii_value(string)
