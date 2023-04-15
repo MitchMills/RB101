a69f23df91aaf43634a80b3d93a1dd83c0c325a5
Mitch Mills <mitchmillsgcp@gmail.com>
2023-04-15 16:49:30 -0500

    Worked on easy5 exercises

--- 00Small_Problems_Exercises/easy5.rb
+++ 00Small_Problems_Exercises/easy5.rb
@@ -1,18 +1,69 @@
-### 2 AFTER MIDNIGHT I
-SECONDS_PER_MINUTE = 60
-MIDNIGHT = Time.new(2023, 4, 16)
 
-def time_of_day(num)
-  (MIDNIGHT + (num * SECONDS_PER_MINUTE)).strftime("%A %R")
+
+### 3 AFTER MIDNIGHT II
+SECONDS_PER_MINUTE = 60
+MIDNIGHT = Time.new(Time.now.year)
+
+def after_midnight(time)
+  MIDNIGHT - time
 end
 
-p time_of_day(0) == "Sunday 00:00"
-p time_of_day(-3) == "Saturday 23:57"
-p time_of_day(35) == "Sunday 00:35"
-p time_of_day(-1437) == "Saturday 00:03"
-p time_of_day(3000) == "Tuesday 02:00"
-p time_of_day(800) == "Sunday 13:20"
-p time_of_day(-4231) == "Thursday 01:29"
+# MINUTES_PER_HOUR = 60
+# MINUTES_PER_DAY = 1440
+
+# def get_total_minutes(time)
+#   hours, minutes = time[0..1].to_i, time[3..4].to_i
+#   total_minutes = (hours * MINUTES_PER_HOUR) + minutes
+# end
+
+# def after_midnight(time)
+#   get_total_minutes(time) % MINUTES_PER_DAY
+# end
+
+# def before_midnight(time)
+#   MINUTES_PER_DAY - get_total_minutes(time)) % MINUTES_PER_DAY
+# end
+
+# def get_total_minutes(time, direction)
+#   hours, minutes = time[0..1].to_i, time[3..4].to_i
+#   if direction == 'after' && hours == 24
+#     hours = 0
+#   elsif direction == 'before' && hours == 0
+#     hours = 24
+#   end
+#   total_minutes = (hours * MINUTES_PER_HOUR) + minutes
+# end
+
+# def after_midnight(time)
+#   get_total_minutes(time, 'after')
+# end
+
+# def before_midnight(time)
+#   MINUTES_PER_DAY - get_total_minutes(time, 'before')
+# end
+
+p after_midnight('00:00') #== 0
+p before_midnight('00:00') #== 0
+p after_midnight('12:34') #== 754
+p before_midnight('12:34') #== 686
+p after_midnight('24:00') #== 0
+p before_midnight('24:00') #== 0
+
+### 2 AFTER MIDNIGHT I
+# SECONDS_PER_MINUTE = 60
+# MIDNIGHT = Time.new(2023, 4, 16)
+
+# def time_of_day(num)
+#   (MIDNIGHT + (num * SECONDS_PER_MINUTE)).strftime("%A %R")
+# end
+
+# p time_of_day(0) == "Sunday 00:00"
+# p time_of_day(-3) == "Saturday 23:57"
+# p time_of_day(35) == "Sunday 00:35"
+# p time_of_day(-1437) == "Saturday 00:03"
+# p time_of_day(3000) == "Tuesday 02:00"
+# p time_of_day(800) == "Sunday 13:20"
+# p time_of_day(-4231) == "Thursday 01:29"
 
 # MINUTES_PER_HOUR = 60
 # MINUTES_PER_DAY = 1440
--- 00Small_Problems_Exercises/pedac_scratchpad.md
+++ 00Small_Problems_Exercises/pedac_scratchpad.md
@@ -1,11 +1,15 @@
 # PROBLEM
+Write two methods that each take a time of day in 24 hour format, and return the number of minutes before and after midnight, respectively. Both methods should return a value in the range 0..1439.
 
+You may not use ruby's Date and Time methods.
 
-  Input: 
-  Output:
+  Input: string (time in 24 hour format)
+  Output: integer (minutes before or after midnight)
 
   ## Rules
   ### explicit
+  - output should be an integer in the range 0..1439
+    - i.e. hours before midnight will not output a negative number
   
   ### implicit
 
@@ -14,13 +18,21 @@
 
 
 # EXAMPLES
-
+after_midnight('00:00') == 0
+before_midnight('00:00') == 0
+after_midnight('12:34') == 754
+before_midnight('12:34') == 686
+after_midnight('24:00') == 0
+before_midnight('24:00') == 0
 
 # DATA STRUCTURES
 
 
 # ALGORITHM
-
+- set a variable `hours` to equal the first two characters in the input string converted to an integer
+- set a variable `minutes` to equal the last two characters in the input string converted to an integer
+- if hours > 23, hours = 0
+- return `(hours * 60) + minutes`
 
 
 
