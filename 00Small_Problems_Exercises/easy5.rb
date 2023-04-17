7a5010d8db2d02f4db643c3a6744cff1525bce5e
Mitch Mills <mitchmillsgcp@gmail.com>
2023-04-16 19:08:53 -0500

    Worked on easy5 exercises

--- 00Small_Problems_Exercises/easy5.rb
+++ 00Small_Problems_Exercises/easy5.rb
@@ -1,9 +1,32 @@
-### LETTER COUNTER I
+### LETTER COUNTER II
 
-p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
-p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
-p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
 p word_sizes('') == {}
+
+### LETTER COUNTER I
+# def word_sizes(string)
+#   sizes = Hash.new(0)
+#   string.split.each { |word| sizes[word.size] += 1 }
+#   sizes
+# end
+
+# def word_sizes(string)
+#   string.split.map {|word| word.size}.tally
+# end
+
+# def word_sizes(string)
+#   string.split.each_with_object(Hash.new(0)) do |word, hash|
+#     hash[word.size] += 1
+#   end
+# end
+
+# p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+# p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
+# p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+# p word_sizes('') == {}
 
 ### 5 CLEAN UP THE WORDS
 # def cleanup(string)
