dc4f260d63155a70ee779dbcdd207a1fb21b2d97
Mitch Mills <mitchmillsgcp@gmail.com>
2023-04-25 15:16:07 -0500

    Refactored calculator

--- 02Small_Programs/Calculator/06_2_calculator.rb
+++ 02Small_Programs/Calculator/06_2_calculator.rb
@@ -1,31 +1,101 @@
-puts "Welcome to Calculator"
-
-print "Please enter the first number: "
-num1 = gets.chomp
-
-print "Please enter the second number: "
-num2 = gets.chomp
-
-puts "Please enter the operation: a) addition, s) subtraction, m) multiplication, d) division"
-operation = gets.chomp
-
-if operation == 'a'
-  operator = '+'
-  result = num1.to_i + num2.to_i
-elsif operation == 's'
-  operator = '-'
-  result = num1.to_i - num2.to_i
-elsif operation == 'm'
-  operator = '*'
-  result = num1.to_i * num2.to_i
-elsif operation == 'd'
-  operator = '/'
-  result = num1.to_f / num2.to_f
+def prompt(message, action = "puts")
+  if action == "print"
+    print "=> #{message}"
+  else
+    puts "=> #{message}"
+  end
 end
 
-puts "#{num1} #{operator} #{num2} = #{result}"
-
-
-def calculate(nums, operation)
-  nums.reduce(operation)
+def get_name
+  loop do
+    prompt("Please enter your name: ", "print")
+    name = gets.chomp
+    return name unless name.empty?
+    puts "Make sure to enter a name."
+  end
 end
+
+def valid_number?(num)
+  num.to_i.to_s == num
+end
+
+def get_number(order)
+  loop do
+    prompt("Please enter the #{order} number: ", "print")
+    number = gets.chomp
+    return number if valid_number?(number)
+    puts "Sorry, that's not a valid number."
+  end
+end
+
+def get_numbers
+  num1 = get_number("first")
+  num2 = get_number("second")
+  [num1, num2]
+end
+
+def operation_to_operator(operation)
+  case operation
+  when 'a' then '+'
+  when 's' then '-'
+  when 'm' then '*'
+  when 'd' then '/'
+  end
+end
+
+def get_operator
+  operation_message = <<~MSG
+  Please enter the operation:
+    a) addition
+    s) subtraction
+    m) multiplication
+    d) division
+  MSG
+
+  prompt(operation_message)
+  loop do
+    operation = gets.chomp
+    operator =  operation_to_operator(operation)
+    return operator if %w(a s m d).include?(operation)
+    prompt("Please enter a valid operation.")
+  end
+end
+
+def get_result(numbers, operator)
+  if operator == '/'
+    numbers.map { |num| num.to_f }.reduce(operator.to_sym)
+  else
+    numbers.map { |num| num.to_i }.reduce(operator.to_sym)
+  end
+end
+
+def display_result(numbers, operator,result)
+  prompt("#{numbers[0]} #{operator} #{numbers[1]} = #{result}")
+end
+
+def another?
+  prompt("Do you want to perform another calcluation?")
+  prompt("If yes, enter 'y'. Enter any other key to exit: ", "print")
+  reply = gets.chomp.downcase
+  reply == 'y'
+end
+
+# Main program loop
+prompt("Welcome to Calculator")
+name = get_name
+prompt("Let's get started, #{name}.")
+
+loop do
+  puts
+  numbers = get_numbers
+  operator = get_operator
+  result = get_result(numbers, operator)
+
+  display_result(numbers, operator, result)
+  puts
+  
+  break unless another?
+end
+
+puts
+prompt("Thank you for using Calculator, #{name}!")
