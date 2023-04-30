c304561ad1df3715e7887feea0209e41667c27d0
Mitch Mills <mitchmillsgcp@gmail.com>
2023-04-30 09:33:07 -0500

    Worked on easy6 exercises

--- 00Small_Problems_Exercises/easy6.rb
+++ 00Small_Problems_Exercises/easy6.rb
@@ -1,19 +1,60 @@
-### 8 FIND THE DUPLICATE
-def find_dup(array)
+### 9 DOES MY LIST INCLUDE THIS?
+def include?(array, target)
   
 end
 
-p find_dup([1, 5, 3, 1]) == 1
-p find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
-          38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
-          14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
-          78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
-          89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
-          41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
-          55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
-          85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
-          40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
-          7,  34, 57, 74, 45, 11, 88, 67,  5, 58]) == 73
+p include?([1,2,3,4,5], 3) == true
+p include?([1,2,3,4,5], 6) == false
+p include?([], 3) == false
+p include?([nil], nil) == true
+p include?([], nil) == false
+
+### 8 FIND THE DUPLICATE
+# def find_dup(array)
+#   array.sort.each_cons(2) do |pair|
+#     return pair[0] if pair[0] == pair[1]
+#   end
+# end
+
+# def find_dup(array)
+#   array.sort!.each_with_index do |num, idx|
+#     return num if num == array[idx + 1]
+#   end
+# end
+
+# def find_dup(array)
+#   array.find { |num| array.count(num) == 2 }
+# end
+
+# def find_dup(array)
+#   array.each_with_object([]) do |num, seen|
+#     return num if seen.include?(num)
+#     seen << num
+#   end
+# end
+
+# def find_dup(array)
+#   array.uniq.each do |num|
+#     array.delete_at(array.index(num))
+#   end
+#   array.first
+# end
+
+# def find_dup(array)
+#   array.sum - array.uniq.sum
+# end
+
+# p find_dup([1, 5, 3, 1]) #== 1
+# p find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+#           38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+#           14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+#           78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+#           89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+#           41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+#           55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+#           85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+#           40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+#           7,  34, 57, 74, 45, 11, 88, 67,  5, 58]) #== 73
 
 ### 7 HALVSIES
 # def halvsies(array)
