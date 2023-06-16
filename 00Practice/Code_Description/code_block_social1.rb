6a4cc74ca92f94726026b5a947fca6726b1d5e68
Mitch Mills <mitchmillsgcp@gmail.com>
2023-06-16 15:44:03 -0500

    Worked on describing code snippets

--- 00Practice/Code_Description/code_block_social1.rb
+++ 00Practice/Code_Description/code_block_social1.rb
@@ -9,14 +9,31 @@ def count_sheep
 end
 
 p count_sheep
+
 ###### NIKHIL FERNANDES, JUNE 12
 
 ### PROBLEM 2
 
 =begin
-The method `count_sheep` is defined on lines 1 through 9 and passed in as an argument to the `Kernel#p` method on line 11.
+On line 11, the `Kernel#p` method is invoked and the method `count_sheep` is passed in as an argument. This method is defined on lines 1 through 11. `Kernel#p` will output the return value of `count_sheep` to the terminal and return that same value.
 
-Within the `count_sheep` method
+Within the `count_sheep` method, on line 2 the `Integer#times` method is called on the integer `5` and a block is passed to it. The method will attempt to execute the block five times, and the integers from 0 through 4 will be passed in to the block on each successive iteration.
+
+Each passed-in integer will be assigned to the block variable `sheep` for the duration of the block's current iteration.
+
+Within the block, on line 3 the `Kernel#puts` method is called and the value assigned to block variable `sheep` is passed in as an argument. This will output the current value of `sheep` to the terminal and return `nil`.
+
+On line 4, a conditional statement will evaluate whether the current value of `sheep` is greater than or equal to 2. If not, the method will proceed to the next iteration of the block. If so, the `return` keyword on line 5 will exit the method.
+
+Line 8 has an implicit return value for the method, a string containing the value "sheep".
+
+When `count_sheep` is called on line 11, the block will be executed three times, and the internal `Kernel#puts` call will output the integers `0`, `1`, and `2` to the terminal on separate lines.
+
+The conditional on line 4 will then evaluate to `true`, and the `return` keyword on line 5 will exit the method. Because the 'return' keyword on line 5 is the last expression evaluated by the method, and it does not include an explicit return value, the method will retun a value of `nil`.
+
+This value is passed as an argument to the `Kernel#p` method invoked on line 11, which will output `nil` to the terminal and return `nil`.
+
+
 
 =end
 
--- 00Practice/Code_Description/code_block_social_slack.rb
+++ 00Practice/Code_Description/code_block_social_slack.rb
@@ -6,15 +6,13 @@ puts n
 
 ###### IAN LEWIS, JUNE 16
 =begin
-On line 1, the local variable `n` is assigned to the return value of invoking the `Integer#times` method on the integer `101` and passing in a block. 
+On line 1, the local variable `n` is assigned to the value returned by invoking the `Integer#times` method on the integer `101` and passing in a block. 
 
-The block itself iterates 101 times, and on each iteration a number from 0 up to 100 is passed in to the block in turn. The block variable `n` is assigned to that value on line 1, so on the first iteration it will be assigned to `0`, on the next it will be assigned to `1`, etc. Within the block, block variable `n` is then reassigned to the value `11` on line 2.
+The block itself iterates 101 times, and on each iteration a number from 0 up to 100 is passed in to the block in turn. The block variable `n` is assigned that value on line 1, so on the first iteration it will be assigned to `0`, on the next it will be assigned to `1`, etc. Within the block, block variable `n` is then reassigned to the value `11` on line 2.
 
 This process repeats 101 times, and then `Integer#times` returns the calling object. So despite all that happens within the block, local variable `n` ends up assigned to `101`.
 
 It's important to note that block variable `n` is scoped to the block and does not affect local variable `n` in the outer scope.
 
 On line 5, the `Kernel#puts` method is called and local variable `n` is passed in as an argument. This outputs the current value of 'n', which is `101`, to the terminal, and returns `nil`.
-
-
 =end
