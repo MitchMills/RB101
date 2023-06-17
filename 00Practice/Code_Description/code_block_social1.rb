6a29a4a7bfe3e4112f638fc7d0e7948bc342a1d4
Mitch Mills <mitchmillsgcp@gmail.com>
2023-06-17 18:36:26 -0500

    Worked on describing code snippets

--- 00Practice/Code_Description/code_block_social1.rb
+++ 00Practice/Code_Description/code_block_social1.rb
@@ -1,23 +1,36 @@
-def count_sheep
-  5.times do |sheep|
-    puts sheep
-    if sheep >= 2
-      return
-    end
-  end
-  "sheep"
+def whats_my_value(value)
+  value = value.upcase!
+  value.concat('!')
 end
 
-p count_sheep
+s = "string"
+t = whats_my_value(s)
+
+p s # => "STRING!"
+p t # => "STRING!"
+
+=begin
+
+=end
 
 ###### NIKHIL FERNANDES, JUNE 12
 
 ### PROBLEM 2
+# def count_sheep
+#   5.times do |sheep|
+#     puts sheep
+#     if sheep >= 2
+#       return
+#     end
+#   end
+#   "sheep"
+# end
 
+# p count_sheep
 =begin
-On line 11, the `Kernel#p` method is invoked and the method `count_sheep` is passed in as an argument. This method is defined on lines 1 through 11. `Kernel#p` will output the return value of `count_sheep` to the terminal and return that same value.
+On line 11, the `Kernel#p` method is invoked and the method `count_sheep` is passed in as an argument. `Kernel#p` will output the return value of `count_sheep` to the terminal and return that same value. To figure out that return value, we have to evaluate the `count_sheep` method, which is defined on lines 1 through 9.
 
-Within the `count_sheep` method, on line 2 the `Integer#times` method is called on the integer `5` and a block is passed to it. The method will attempt to execute the block five times, and the integers from 0 through 4 will be passed in to the block on each successive iteration.
+Within the `count_sheep` method, on line 2 the `Integer#times` method is called on the integer `5` and a block is passed to it. The method will attempt to execute the block five times, and the integers 0 through 4 will be passed in to the block on each successive iteration.
 
 Each passed-in integer will be assigned to the block variable `sheep` for the duration of the block's current iteration.
 
@@ -25,20 +38,18 @@ Within the block, on line 3 the `Kernel#puts` method is called and the value ass
 
 On line 4, a conditional statement will evaluate whether the current value of `sheep` is greater than or equal to 2. If not, the method will proceed to the next iteration of the block. If so, the `return` keyword on line 5 will exit the method.
 
-Line 8 has an implicit return value for the method, a string containing the value "sheep".
+So when `count_sheep` is called on line 11, the block will be executed three times, and the internal `Kernel#puts` call will output the integers `0`, `1`, and `2` to the terminal on separate lines.
 
-When `count_sheep` is called on line 11, the block will be executed three times, and the internal `Kernel#puts` call will output the integers `0`, `1`, and `2` to the terminal on separate lines.
+Because the current value of `sheep` will then be equal to 2, the conditional on line 4 will evaluate to `true`, and the `return` keyword on line 5 will exit the method. Line 8 of the method will never be executed.
 
-The conditional on line 4 will then evaluate to `true`, and the `return` keyword on line 5 will exit the method. Because the 'return' keyword on line 5 is the last expression evaluated by the method, and it does not include an explicit return value, the method will retun a value of `nil`.
+Because the 'return' keyword on line 5 is the last expression evaluated by the method, and it does not include an explicit return value, the method will implicitly return a value of `nil`.
 
-This value is passed as an argument to the `Kernel#p` method invoked on line 11, which will output `nil` to the terminal and return `nil`.
-
-
+This return value is passed as an argument to the `Kernel#p` method invoked on line 11, which will output `nil` to the terminal and return `nil`.
 
 =end
 
 
-### PROBLEM 1
+###### PROBLEM 1
 # arr = ["b", "a"]
 # arr = arr.product([Array(1..3)])
 # arr.first.delete(arr.first.last)
