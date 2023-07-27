1ca348ed6811b3bddae5798b34807da084955ead
Mitch Mills <mitchmillsgcp@gmail.com>
2023-07-27 07:07:38 -0500

    Reworked Practice Problems

--- 03Practice_Problems/easy2.rb
+++ 03Practice_Problems/easy2.rb
@@ -1,9 +1,42 @@
+### Question 7.2
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+# flintstones << "Dino" << "Hoppy"
+# flintstones += %w(Dino Hoppy)
+# flintstones.concat(%w(Dino Hoppy))
+# flintstones.push("Dino", "Hoppy")
+# flintstones.append("Dino", "Hoppy")
+p flintstones
+
+### Question 6.2
+# flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+# flintstones << "Dino"
+# flintstones += ["Dino"]
+# flintstones.concat(["Dino"])
+# flintstones.push("Dino")
+# flintstones.append("Dino")
+# p flintstones
+
+### Question 5.2
+# flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+# flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+### Question 4.2
+# advice = "Few things in life are as important as house training your pet dinosaur."
+# p advice.match?("Dino")
+
+### Question 3.2
+# ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
+# additional_ages = { "Marilyn" => 22, "Spot" => 237 }
+# ages.merge!(additional_ages)
+# p ages
+
 ### Question 2.2
-munsters_description = "The Munsters are creepy in a good way."
-"tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
-"The munsters are creepy in a good way."
-"the munsters are creepy in a good way."
-"THE MUNSTERS ARE CREEPY IN A GOOD WAY."
+# munsters_description = "The Munsters are creepy in a good way."
+
+# p munsters_description.swapcase
+# p munsters_description.capitalize
+# p munsters_description.downcase
+# p munsters_description.upcase
 
 ### Question 1.2
 # ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
