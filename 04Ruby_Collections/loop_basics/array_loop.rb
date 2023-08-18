466f87789971f291638278ec8c40b62e92b78d36
Mitch Mills <mitchmillsgcp@gmail.com>
2023-08-18 14:59:27 -0500

    Set up RB110 files

--- 04Ruby_Collections/loop_basics/array_loop.rb
+++
@@ -1,8 +0,0 @@
-array = ['a', 'b', 'c', 'd', 'e']
-counter = 0
-
-loop do
-  break if counter >= array.size
-  puts array[counter]
-  counter += 1
-end
--- 04Ruby_Collections/loop_basics/string_loop.rb
+++
@@ -1,8 +0,0 @@
-string = "abcdefg"
-counter = 0
-
-loop do
-  break if counter >= string.size
-  puts string[counter]
-  counter += 1
-end
