c49a6943b333da2bc719dbb9a5902da56fd99d24
Mitch Mills <mitchmillsgcp@gmail.com>
2023-08-18 12:17:40 -0500

    Reworked easy 5 exercises

--- 00Practice/Small_Problems_Exercises/easy5.rb
+++ 00Practice/Small_Problems_Exercises/easy5.rb
@@ -1,12 +1,54 @@
-### 6.2 LETTER COUNTER I
+### 7.2 LETTER COUNTER II
 def word_sizes(string)
 
 end
 
-p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
-p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
-p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
 p word_sizes('') == {}
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+
+### 6.2 LETTER COUNTER I
+# def word_sizes(words)
+#   sizes = words.split.map(&:size)
+#   counts = sizes.uniq.map { |size| sizes.count(size) }
+#   sizes.uniq.zip(counts).to_h
+# end
+
+# def word_sizes(string)
+#   sizes = string.split.map(&:size)
+#   sizes.each_with_object({}) { |size, result| result[size] = sizes.count(size) }
+# end
+
+# def word_sizes(string)
+#   sizes = string.split.map(&:size)
+#   sizes.map { |size| [size, sizes.count(size)] }.to_h
+# end
+
+###
+# def word_sizes(string)
+#   sizes = get_word_sizes(string)
+#   get_size_counts(sizes)
+# end
+
+# def get_word_sizes(string)
+#   string.split.map(&:size)
+# end
+
+# def get_size_counts(sizes)
+#   sizes.map { |size| [size, sizes.count(size)] }.to_h
+# end
+###
+
+# def word_sizes(string)
+#   string.split.map(&:size).tally
+# end
+
+# p word_sizes('Four score and seven.')# == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+# p word_sizes('Hey diddle diddle, the cat and the fiddle!')# == { 3 => 5, 6 => 1, 7 => 2 }
+# p word_sizes("What's up doc?")# == { 6 => 1, 2 => 1, 4 => 1 }
+# p word_sizes('')# == {}
 
 ### 5.2 CLEAN UP THE WORDS
 # def cleanup(string)
--- 00Practice/Small_Problems_Exercises/pedac_scratchpad.md
+++ 00Practice/Small_Problems_Exercises/pedac_scratchpad.md
@@ -1,7 +1,5 @@
 # PROBLEM
-Write a method that takes a string with one or more space separated words and returns a hash that shows the number of words of different sizes.
-
-Words consist of any string of characters that do not include a space.
+Modify the word_sizes method from the previous exercise to exclude non-letters when determining word size. For instance, the length of "it's" is 3, not 4.
 
   Input: 
 
@@ -17,16 +15,15 @@ Words consist of any string of characters that do not include a space.
 
 
 # EXAMPLES
-word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
-word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
-word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
 word_sizes('') == {}
 
 # DATA STRUCTURES
 
 
 # ALGORITHM
-
 
 # # # # # # # # # # # # # # # # # # # # # # # # #
 # # # # # # # # # # # # # # # # # # # # # # # # #
